2f1610af5669e3583f60c1787a29efd011fdc3fd
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2019-11-20 18:26:16 +0600

    wabtn
    20.11.2019
    18.26

--- wabtn/js/wa-btn_script.js
+++ wabtn/js/wa-btn_script.js
@@ -6,5 +6,8 @@ $(document).ready(function() {
 	$(".wa-btn__link").mouseleave(function(){
 		$(".wa-btn__note").removeClass('wa-btn__note--animation');
 	});
+	$(".wa-btn__link").click(function(){
+		$(".wa-btn__note").removeClass('wa-btn__note--animation');
+	});
 
 });
