4560c7b038dcee147da29e80dca437dd8e786c81
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2019-11-24 23:14:58 +0600

    room
    24.11.2019
    23.14

--- room/js/scripts.js
+++ room/js/scripts.js
@@ -12,11 +12,19 @@ $(document).ready(function() {
 	});
 
 	$(".room__hover__bed").click(function(){
-		$(".room__hover__bed, .room__hover__locker, .room__hover__table, .room__hover__stand, .room__things").fadeOut(0);
+		$(".room__hover__bed").fadeOut(0);
 		$(".room__img__bed").fadeIn(400);
-		$(".room__dark-matter").delay(400).fadeIn(350);
-		$(".room__popup__bed").delay(400).fadeIn(350);
+		$(".room__things__scooter").fadeIn(400);
+/* 		$(".room__dark-matter").delay(400).fadeIn(350);
+		$(".room__popup__bed").delay(400).fadeIn(350); */
+
+		$(".room__things__scooter").delay(15000).fadeOut(0);
+		$(".room__img__bed").delay(15000).fadeOut(400);
+		$(".room__hover__bed").delay(15500).fadeIn(400);
 	});
+
+
+
 	$(".room__hover__table").click(function(){
 		$(".room__hover__bed, .room__hover__locker, .room__hover__table, .room__hover__stand, .room__things").fadeOut(0);
 		$(".room__img__table").fadeIn(400);
@@ -40,74 +48,84 @@ $(document).ready(function() {
 		$(".room__things").delay(400).fadeIn(0);
 	});
 	
-	$(".room__popup__locker__flex__copter").mouseenter(function(){
-		$(".room__popup__locker__flex__note").fadeIn(300).css({display: "flex"});
+
+
+
+	$(".room__popup__locker__copter").mouseenter(function(){
+		$(".room__popup__locker__copter__note").stop().fadeIn(300);
 	});
-	$(".room__popup__locker__flex__copter").mouseleave(function(){
-		$(".room__popup__locker__flex__note").fadeOut(300);
+	$(".room__popup__locker__copter").mouseleave(function(){
+		$(".room__popup__locker__copter__note").stop().fadeOut(300);
 	});
 
 
 	$(".room__popup__table__iphone").mouseenter(function(){
-		$(".room__popup__table__iphone__note").fadeIn(300);
+		$(".room__popup__table__iphone__note").stop().fadeIn(300);
 	});
 	$(".room__popup__table__iphone").mouseleave(function(){
-		$(".room__popup__table__iphone__note").fadeOut(300);
+		$(".room__popup__table__iphone__note").stop().fadeOut(300);
 	});
 
 
 	$(".room__popup__table__macbook").mouseenter(function(){
-		$(".room__popup__table__macbook__note").fadeIn(300);
+		$(".room__popup__table__macbook__note").stop().fadeIn(300);
 	});
 	$(".room__popup__table__macbook").mouseleave(function(){
-		$(".room__popup__table__macbook__note").fadeOut(300);
+		$(".room__popup__table__macbook__note").stop().fadeOut(300);
 	});
 
 
 	$(".room__popup__stand__photocam").mouseenter(function(){
-		$(".room__popup__stand__photocam__note").fadeIn(300);
+		$(".room__popup__stand__photocam__note").stop().fadeIn(300);
 	});
 	$(".room__popup__stand__photocam").mouseleave(function(){
-		$(".room__popup__stand__photocam__note").fadeOut(300);
+		$(".room__popup__stand__photocam__note").stop().fadeOut(300);
 	});
 
 
 	$(".room__popup__stand__gopro").mouseenter(function(){
-		$(".room__popup__stand__gopro__note").fadeIn(300);
+		$(".room__popup__stand__gopro__note").stop().fadeIn(300);
 	});
 	$(".room__popup__stand__gopro").mouseleave(function(){
-		$(".room__popup__stand__gopro__note").fadeOut(300);
+		$(".room__popup__stand__gopro__note").stop().fadeOut(300);
 	});
 
 
 
 
 	$(".room__things__piano__hover").mouseenter(function(){
-		$(".room__things__piano__note").fadeIn(300);
+		$(".room__things__piano__note").stop().fadeIn(300);
 	});
 	$(".room__things__piano__hover").mouseleave(function(){
-		$(".room__things__piano__note").fadeOut(300);
+		$(".room__things__piano__note").stop().fadeOut(300);
 	});
 
 	$(".room__things__telescope__hover").mouseenter(function(){
-		$(".room__things__telescope__note").fadeIn(300);
+		$(".room__things__telescope__note").stop().fadeIn(300);
 	});
 	$(".room__things__telescope__hover").mouseleave(function(){
-		$(".room__things__telescope__note").fadeOut(300);
+		$(".room__things__telescope__note").stop().fadeOut(300);
+	});
+
+	$(".room__things__scooter__hover").mouseenter(function(){
+		$(".room__things__scooter__note").stop().fadeIn(300);
+	});
+	$(".room__things__scooter__hover").mouseleave(function(){
+		$(".room__things__scooter__note").stop().fadeOut(300);
 	});
 
 	$(".room__things__sound__hover1, .room__things__sound__hover2").mouseenter(function(){
-		$(".room__things__sound__note").fadeIn(300);
+		$(".room__things__sound__note").stop().fadeIn(300);
 	});
 	$(".room__things__sound__hover1, .room__things__sound__hover2").mouseleave(function(){
-		$(".room__things__sound__note").fadeOut(300);
+		$(".room__things__sound__note").stop().fadeOut(300);
 	});
 
 	$(".room__things__run__hover1, .room__things__run__hover2").mouseenter(function(){
-		$(".room__things__run__note").fadeIn(300);
+		$(".room__things__run__note").stop().fadeIn(300);
 	});
 	$(".room__things__run__hover1, .room__things__run__hover2").mouseleave(function(){
-		$(".room__things__run__note").fadeOut(300);
+		$(".room__things__run__note").stop().fadeOut(300);
 	});
 
 });
