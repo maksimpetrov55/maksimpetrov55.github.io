c2f9d74938905cf5da1d97e7df27baed9690561b
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2019-12-19 20:08:39 +0600

    tepl
    19.12.2019
    20.08

---
+++ tepl/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+
+	// $('.about__img__point').hover(function(event) {
+	// 	$('.about__img__point--hover').removeClass('about__img__point--hover');
+	// 	$('.about__img__point--hover').next().removeClass('about__img__text--hover');
+	// 		$(this).addClass('about__img__point--hover');
+	// 		$(this).next().addClass('about__img__text--hover');
+	// });
+
+
+});
