4957c945bb111673a845ee3df14e18fc775552a6
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2020-01-15 17:34:30 +0600

    stol
    15.01.2020
    17.34

--- stol/js/test.js
+++ stol/js/test.js
@@ -1,3 +1,12 @@
+$(document).ready(function() {
 
+    function auto_H() {
+        var H_param = $(".calc").height();
+        console.log(H_param);
+    }
+    auto_H();
+    $(window).resize(function(){
+        auto_H();
+    });
 
-alert("hello");
+});
