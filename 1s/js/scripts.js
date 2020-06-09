dcfc35c8d68700d7aa7fe4301f16f675c64ea6dc
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2020-06-09 12:56:55 +0600

    1s
    09.06.2020
    12.56

--- 1s/js/scripts.js
+++ 1s/js/scripts.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
 	//плавный скролл до якоря
 
 	//экран загрузки
-	//$(".loading__logo").delay(100).fadeOut(100);
-	//$(".loading").delay(200).fadeOut(130);
+	// $(".loading__logo").delay(100).fadeOut(100);
+	// $(".loading").delay(200).fadeOut(130);
 	$(".loading__logo").delay(1000).fadeOut(1000);
 	$(".loading").delay(2000).fadeOut(1300);
 	//экран загрузки
@@ -39,7 +39,7 @@ $(document).ready(function() {
 
 	//маска телефона
 	$(function(){
-		//$("").mask("+7 (999) 999-99-99", {placeholder: "__________" });
+		$(".phone-mask").mask("+7 (999) 999-99-99", {placeholder: "__________" });
 	});
 	//маска телефона
 
@@ -56,15 +56,21 @@ $(document).ready(function() {
 			$('.header').addClass("header--scroll");
 			$('.header__logo').addClass("header__logo--scroll");
 			$('.header__separator').addClass("header__separator--scroll");
+			$('.header__menu').addClass("header__menu--scroll");
 			$('.header__link').addClass("header__link--scroll");
 			$('.header__phone').addClass("header__phone--scroll");
+			$('.header__burger-line').addClass("header__burger-line--scroll");
+			$('.header__icon').addClass("header__icon--scroll");
 		} else {
 			$('.header').removeClass("header--show");
 			$('.header').removeClass("header--scroll");
 			$('.header__logo').removeClass("header__logo--scroll");
 			$('.header__separator').removeClass("header__separator--scroll");
+			$('.header__menu').removeClass("header__menu--scroll");
 			$('.header__link').removeClass("header__link--scroll");
 			$('.header__phone').removeClass("header__phone--scroll");
+			$('.header__burger-line').removeClass("header__burger-line--scroll");
+			$('.header__icon').removeClass("header__icon--scroll");
 		}
 
 		if ( scroll > 500 ) {
@@ -79,6 +85,20 @@ $(document).ready(function() {
 		headerChange();
 	});
 	//прилипание хедера
+
+	//бургер меню
+	$('.header__burger').click(function(){
+		$('.header__burger').toggleClass("header__burger--close");
+		$('.header__menu').toggleClass("header__menu--show");
+	});
+	$('.header__link').click(function(){
+		var dWidth = $( document ).width();
+		if ( dWidth <= 960 ) {
+			$('.header__burger').toggleClass("header__burger--close");
+			$('.header__menu').toggleClass("header__menu--show");
+		}
+	});
+	//бургер меню
 
 
 });
