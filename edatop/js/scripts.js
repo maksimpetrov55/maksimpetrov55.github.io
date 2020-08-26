74793915b2f75708dd35444eb7c99ee7d3577ed8
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2020-08-27 00:17:27 +0600

    edatop
    27.08.2020
    0.17

--- edatop/js/scripts.js
+++ edatop/js/scripts.js
@@ -58,13 +58,65 @@ $(document).ready(function() {
 			}
 	
 			var calc_formula_1 = ( (calc_wei * 9.99) + (calc_hei * 6.25) - ( calc_age * 4.92 ) + calc_k_1 ) * calc_k_2;
+			var calc_formula_3 = calc_wei * calc_k_2;
 	
 			$('#cacl_formula_1').text("По формуле Миффлина-Сан Жеора: " + calc_formula_1.toFixed(0) );
 			$('#cacl_formula_2').text("По формуле Харриса-Бенедикта: " + calc_formula_2.toFixed(0) );
+			$('#cacl_formula_3').text("По среднему расходу на килограмм: " + (calc_formula_3 * 22).toFixed(0) + " – " + (calc_formula_3 * 26).toFixed(0) );
 
-			if ( calc_goal == 1 ) { $('#calc_goal_title').text("Ориентиры для сброса веса:"); }
-			if ( calc_goal == 2 ) { $('#calc_goal_title').text("Ориентиры для набора мышечной массы:"); }
-			if ( calc_goal == 3 ) { $('#calc_goal_title').text("Ориентиры для поддержания веса:"); }
+			if ( calc_goal == 1 ) { 
+				$('#calc_goal_title').text("Ориентиры для сброса веса:");
+				$('#calc_text_goal').text("Это пример текста который зависит от цели. Сейчас цель – сбросить вес.");
+				var calc_formula_4 = (calc_formula_1 + calc_formula_2) / 2;
+				$('#calc_formula_4').text("Диапазон калорий: " + (calc_formula_4 * 0.8).toFixed(0) + " – " + (calc_formula_4 * 0.9).toFixed(0) );
+				$('#calc_formula_5').text("Суточная норма белка: " + (calc_formula_4 * 0.85 * 0.3 / 4).toFixed(0) + " грамм");
+				$('#calc_formula_6').text("Суточная норма жиров: " + (calc_formula_4 * 0.85 * 0.2 / 9).toFixed(0) + " грамм");
+				$('#calc_formula_7').text("Суточная норма углеводов: " + (calc_formula_4 * 0.85 * 0.5 / 4).toFixed(0) + " грамм");
+
+				var calc_text_param = calc_formula_4 * 0.9;
+			}
+			if ( calc_goal == 2 ) { 
+				$('#calc_goal_title').text("Ориентиры для набора мышечной массы:");
+				$('#calc_text_goal').text("Это пример текста который зависит от цели. Сейчас цель – набрать мышечную массу.");
+				var calc_formula_4 = (calc_formula_1 + calc_formula_2) / 2;
+				$('#calc_formula_4').text("Диапазон калорий: " + (calc_formula_4 * 1.1).toFixed(0) + " – " + (calc_formula_4 * 1.2).toFixed(0) );
+				$('#calc_formula_5').text("Суточная норма белка: " + (calc_formula_4 * 1.15 * 0.3 / 4).toFixed(0) + " грамм");
+				$('#calc_formula_6').text("Суточная норма жиров: " + (calc_formula_4 * 1.15 * 0.3 / 9).toFixed(0) + " грамм");
+				$('#calc_formula_7').text("Суточная норма углеводов: " + (calc_formula_4 * 1.15 * 0.4 / 4).toFixed(0) + " грамм");
+
+				var calc_text_param = calc_formula_4 * 1.2;
+			}
+			if ( calc_goal == 3 ) { 
+				$('#calc_goal_title').text("Ориентиры для поддержания веса:");
+				$('#calc_text_goal').text("Это пример текста который зависит от цели. Сейчас цель – поддерживать вес.");
+				var calc_formula_4 = (calc_formula_1 + calc_formula_2) / 2;
+				$('#calc_formula_4').text("Диапазон калорий: " + (calc_formula_4 * 0.9).toFixed(0) + " – " + calc_formula_4.toFixed(0) );
+				$('#calc_formula_5').text("Суточная норма белка: " + (calc_formula_4 * 0.95 * 0.3 / 4).toFixed(0) + " грамм");
+				$('#calc_formula_6').text("Суточная норма жиров: " + (calc_formula_4 * 0.95 * 0.3 / 9).toFixed(0) + " грамм");
+				$('#calc_formula_7').text("Суточная норма углеводов: " + (calc_formula_4 * 0.95 * 0.4 / 4).toFixed(0) + " грамм");
+
+				var calc_text_param = calc_formula_4 * 1;
+			}
+
+			if ( calc_text_param < 1500 ) { $('#calc_text_value').text( "Это пример текста который зависит от диапазона калорий. Сейчас диапазон до 1500." ); 
+			} else {
+				if ( calc_text_param < 2000 ) { $('#calc_text_value').text( "Это пример текста который зависит от диапазона калорий. Сейчас диапазон до 2000." ); 
+				} else {
+					if ( calc_text_param < 2500 ) { $('#calc_text_value').text( "Это пример текста который зависит от диапазона калорий. Сейчас диапазон до 2500." ); 
+					} else {
+						if ( calc_text_param < 3000 ) { $('#calc_text_value').text( "Это пример текста который зависит от диапазона калорий. Сейчас диапазон до 3000." ); 
+						} else {
+							if ( calc_text_param < 3500 ) { $('#calc_text_value').text( "Это пример текста который зависит от диапазона калорий. Сейчас диапазон до 3500." ); 
+							} else {
+								if ( calc_text_param < 4000 ) { $('#calc_text_value').text( "Это пример текста который зависит от диапазона калорий. Сейчас диапазон до 4000." ); 
+								} else {
+									if ( calc_text_param < 4500 ) { $('#calc_text_value').text( "Это пример текста который зависит от диапазона калорий. Сейчас диапазон до 4500." ); 
+									} else {
+										if ( calc_text_param < 5000 ) { $('#calc_text_value').text( "Это пример текста который зависит от диапазона калорий. Сейчас диапазон до 5000." ); 
+										} else {
+											if ( calc_text_param >= 5000 ) { $('#calc_text_value').text( "Это пример текста который зависит от диапазона калорий. Сейчас диапазон от 5000." ); 
+											}		}		}		}		}		}		}		}		}
+
 
 		} else {
 			$('.calc__warning').addClass("calc__warning--show");
