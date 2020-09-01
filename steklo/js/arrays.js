359ea1e92e560b0b9210ef21f5dcf318835ffe29
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2020-09-01 17:13:59 +0600

    steklo
    01.09.2020
    17.13

---
+++ steklo/js/arrays.js
@@ -0,0 +1,246 @@
+
+
+
+var glassArr = [
+    {id:1, name: "Стекло обычное 2 мм", type: "Стекло обычное", value: "500", thin: "2"},
+    {id:2, name: "Стекло обычное 4 мм", type: "Стекло обычное", value: "825", thin: "4"},
+    {id:3, name: "Стекло обычное 5 мм", type: "Стекло обычное", value: "975", thin: "5"},
+    {id:4, name: "Стекло обычное 6 мм", type: "Стекло обычное", value: "1275", thin: "6"},
+    {id:5, name: "Стекло обычное 8 мм", type: "Стекло обычное", value: "1787", thin: "8"},
+    {id:6, name: "Стекло обычное 10 мм", type: "Стекло обычное", value: "2125", thin: "10"},
+    {id:7, name: "Стекло обычное 12 мм", type: "Стекло обычное", value: "3400", thin: "12"},
+    {id:8, name: "Стекло обычное 15 мм", type: "Стекло обычное", value: "7480", thin: "15"},
+    {id:9, name: "Стекло обычное 19 мм", type: "Стекло обычное", value: "11900", thin: "19"},
+    {id:10, name: "Стекло матовое (Сатин) 4 мм", type: "Стекло матовое (Сатин)", value: "1445", thin: "4"},
+    {id:11, name: "Стекло матовое (Сатин) 6 мм", type: "Стекло матовое (Сатин)", value: "2210", thin: "6"},
+    {id:12, name: "Стекло матовое (Сатин) 8 мм", type: "Стекло матовое (Сатин)", value: "3230", thin: "8"},
+    {id:13, name: "Стекло матовое (Сатин) 10 мм", type: "Стекло матовое (Сатин)", value: "3910", thin: "10"},
+    {id:14, name: "Стекло бронза 4 мм", type: "Стекло бронза", value: "1785", thin: "4"},
+    {id:15, name: "Стекло бронза 6 мм", type: "Стекло бронза", value: "2550", thin: "6"},
+    {id:16, name: "Стекло бронза 8 мм", type: "Стекло бронза", value: "3230", thin: "8"},
+    {id:17, name: "Стекло бронза 10 мм", type: "Стекло бронза", value: "4250", thin: "10"},
+    {id:18, name: "Стекло бронза 12 мм", type: "Стекло бронза", value: "5780", thin: "12"},
+    {id:19, name: "Стекло графит 4 мм", type: "Стекло графит", value: "1445", thin: "4"},
+    {id:20, name: "Стекло графит 6 мм", type: "Стекло графит", value: "2210", thin: "6"},
+    {id:21, name: "Стекло графит 8 мм", type: "Стекло графит", value: "3825", thin: "8"},
+    {id:22, name: "Стекло графит 10 мм", type: "Стекло графит", value: "4250", thin: "10"},
+    {id:23, name: "Стекло сатин бронза 4 мм", type: "Стекло сатин бронза", value: "2720", thin: "4"},
+    {id:24, name: "Стекло сатин бронза 6 мм", type: "Стекло сатин бронза", value: "3570", thin: "6"},
+    {id:25, name: "Стекло сатин бронза 8 мм", type: "Стекло сатин бронза", value: "4930", thin: "8"},
+    {id:26, name: "Стекло сатин бронза 10 мм", type: "Стекло сатин бронза", value: "5950", thin: "10"},
+    {id:27, name: "Стекло осветленное 4 мм", type: "Стекло осветленное", value: "2960", thin: "4"},
+    {id:28, name: "Стекло осветленное 5 мм", type: "Стекло осветленное", value: "3700", thin: "5"},
+    {id:29, name: "Стекло осветленное 6 мм", type: "Стекло осветленное", value: "4200", thin: "6"},
+    {id:30, name: "Стекло осветленное 8 мм", type: "Стекло осветленное", value: "6200", thin: "8"},
+    {id:31, name: "Стекло осветленное 10 мм", type: "Стекло осветленное", value: "7500", thin: "10"},
+    {id:32, name: "Стекло осветленное 12 мм", type: "Стекло осветленное", value: "10000", thin: "12"},
+    {id:33, name: "Зеркало серебро 4 мм", type: "Зеркало серебро", value: "1500", thin: "4"},
+    {id:34, name: "Зеркало серебро 5 мм", type: "Зеркало серебро", value: "2040", thin: "5"},
+    {id:35, name: "Зеркало серебро 6 мм", type: "Зеркало серебро", value: "2550", thin: "6"},
+    {id:36, name: "Зеркало бронза 4 мм", type: "Зеркало бронза", value: "2431", thin: "4"},
+    {id:37, name: "Зеркало графит 4 мм", type: "Зеркало графит", value: "2431", thin: "4"},
+    {id:38, name: "Зеркало золото 4 мм", type: "Зеркало золото", value: "6460", thin: "4"},
+    {id:39, name: "Зеркало осветленное 4 мм", type: "Зеркало осветленное", value: "4930", thin: "4"},
+    {id:40, name: "Зеркало осветленное 6 мм", type: "Зеркало осветленное", value: "6630", thin: "6"},
+    {id:41, name: "Зеркало состаренное 4 мм", type: "Зеркало состаренное", value: "10200", thin: "4"},
+    {id:42, name: "Триплекс листовой 4+4 мм", type: "Триплекс листовой", value: "2805", thin: "4+4"},
+    {id:43, name: "Триплекс листовой 5+5 мм", type: "Триплекс листовой", value: "3400", thin: "5+5"}
+]
+
+var obrabotka_1_Arr = [
+    {thin: "2", value: "0"},
+    {thin: "4", value: "90"},
+    {thin: "5", value: "100"},
+    {thin: "6", value: "120"},
+    {thin: "8", value: "200"},
+    {thin: "10", value: "190"},
+    {thin: "12", value: "370"},
+    {thin: "15", value: "410"},
+    {thin: "19", value: "630"},
+    {thin: "4+4", value: "340"},
+    {thin: "5+5", value: "600"},
+    {thin: "6+6", value: "600"}
+]
+
+var obrabotka_2_Arr = [
+    {thin: "2", value: "0"},
+    {thin: "4", value: "180"},
+    {thin: "5", value: "200"},
+    {thin: "6", value: "220"},
+    {thin: "8", value: "320"},
+    {thin: "10", value: "340"},
+    {thin: "12", value: "600"},
+    {thin: "15", value: "750"},
+    {thin: "19", value: "900"},
+    {thin: "4+4", value: "680"},
+    {thin: "5+5", value: "1140"},
+    {thin: "6+6", value: "1140"}
+]
+
+var obrabotka_3_Arr = [
+    {thin: "2", value: "0"},
+    {thin: "4", value: "160"},
+    {thin: "5", value: "180"},
+    {thin: "6", value: "200"},
+    {thin: "8", value: "240"},
+    {thin: "10", value: "260"},
+    {thin: "12", value: "540"},
+    {thin: "15", value: "690"},
+    {thin: "19", value: "890"},
+    {thin: "4+4", value: "500"},
+    {thin: "5+5", value: "1000"},
+    {thin: "6+6", value: "1000"}
+]
+
+var obrabotka_4_Arr = [
+    {thin: "2", value: "0"},
+    {thin: "4", value: "280"},
+    {thin: "5", value: "290"},
+    {thin: "6", value: "300"},
+    {thin: "8", value: "580"},
+    {thin: "10", value: "600"},
+    {thin: "12", value: "1160"},
+    {thin: "15", value: "1440"},
+    {thin: "19", value: "1800"},
+    {thin: "4+4", value: "960"},
+    {thin: "5+5", value: "1800"},
+    {thin: "6+6", value: "1800"}
+]
+
+var obrabotka_5_Arr = [
+    {thin: "2", value: "0"},
+    {thin: "4", value: "170"},
+    {thin: "5", value: "180"},
+    {thin: "6", value: "200"},
+    {thin: "8", value: "370"},
+    {thin: "10", value: "370"},
+    {thin: "12", value: "880"},
+    {thin: "15", value: "0"},
+    {thin: "19", value: "0"},
+    {thin: "4+4", value: "0"},
+    {thin: "5+5", value: "0"},
+    {thin: "6+6", value: "0"}
+]
+
+var obrabotka_6_Arr = [
+    {thin: "2", value: "0"},
+    {thin: "4", value: "370"},
+    {thin: "5", value: "370"},
+    {thin: "6", value: "400"},
+    {thin: "8", value: "490"},
+    {thin: "10", value: "660"},
+    {thin: "12", value: "1100"},
+    {thin: "15", value: "0"},
+    {thin: "19", value: "0"},
+    {thin: "4+4", value: "0"},
+    {thin: "5+5", value: "0"},
+    {thin: "6+6", value: "0"}
+]
+
+var zakalka_Arr = [
+    {thin: "0", value: "0"},
+    {thin: "2", value: "0"},
+    {thin: "4", value: "1485"},
+    {thin: "5", value: "1710"},
+    {thin: "6", value: "1710"},
+    {thin: "8", value: "1935"},
+    {thin: "10", value: "2160"},
+    {thin: "12", value: "3150"},
+    {thin: "15", value: "0"},
+    {thin: "19", value: "0"},
+    {thin: "4+4", value: "0"},
+    {thin: "5+5", value: "0"},
+    {thin: "6+6", value: "0"}
+]
+
+
+var hole_1_Arr = [
+    {thin: "2", value: "0"},
+    {thin: "4", value: "130"},
+    {thin: "5", value: "130"},
+    {thin: "6", value: "130"},
+    {thin: "8", value: "160"},
+    {thin: "10", value: "160"},
+    {thin: "12", value: "160"},
+    {thin: "15", value: "205"},
+    {thin: "19", value: "205"},
+    {thin: "4+4", value: "260"},
+    {thin: "5+5", value: "260"},
+    {thin: "6+6", value: "260"}
+]
+
+var hole_2_Arr = [
+    {thin: "2", value: "0"},
+    {thin: "4", value: "160"},
+    {thin: "5", value: "160"},
+    {thin: "6", value: "160"},
+    {thin: "8", value: "205"},
+    {thin: "10", value: "205"},
+    {thin: "12", value: "205"},
+    {thin: "15", value: "250"},
+    {thin: "19", value: "250"},
+    {thin: "4+4", value: "320"},
+    {thin: "5+5", value: "320"},
+    {thin: "6+6", value: "320"}
+]
+
+var hole_3_Arr = [
+    {thin: "2", value: "0"},
+    {thin: "4", value: "250"},
+    {thin: "5", value: "250"},
+    {thin: "6", value: "250"},
+    {thin: "8", value: "310"},
+    {thin: "10", value: "310"},
+    {thin: "12", value: "310"},
+    {thin: "15", value: "400"},
+    {thin: "19", value: "400"},
+    {thin: "4+4", value: "500"},
+    {thin: "5+5", value: "500"},
+    {thin: "6+6", value: "500"}
+]
+
+var hole_4_Arr = [
+    {thin: "2", value: "0"},
+    {thin: "4", value: "1420"},
+    {thin: "5", value: "1420"},
+    {thin: "6", value: "1420"},
+    {thin: "8", value: "1420"},
+    {thin: "10", value: "1420"},
+    {thin: "12", value: "1420"},
+    {thin: "15", value: "1420"},
+    {thin: "19", value: "1420"},
+    {thin: "4+4", value: "2840"},
+    {thin: "5+5", value: "2840"},
+    {thin: "6+6", value: "2840"}
+]
+
+var calc_inner_Arr = [
+    {thin: "2", value: "0"},
+    {thin: "4", value: "750"},
+    {thin: "5", value: "1000"},
+    {thin: "6", value: "1000"},
+    {thin: "8", value: "1125"},
+    {thin: "10", value: "1250"},
+    {thin: "12", value: "1750"},
+    {thin: "15", value: "0"},
+    {thin: "19", value: "0"},
+    {thin: "4+4", value: "0"},
+    {thin: "5+5", value: "0"},
+    {thin: "6+6", value: "0"}
+]
+
+var calc_outer_Arr = [
+    {thin: "2", value: "0"},
+    {thin: "4", value: "510"},
+    {thin: "5", value: "680"},
+    {thin: "6", value: "680"},
+    {thin: "8", value: "765"},
+    {thin: "10", value: "850"},
+    {thin: "12", value: "1190"},
+    {thin: "15", value: "0"},
+    {thin: "19", value: "0"},
+    {thin: "4+4", value: "0"},
+    {thin: "5+5", value: "0"},
+    {thin: "6+6", value: "0"}
+]
+
---
+++ steklo/js/scripts.js
@@ -0,0 +1,358 @@
+$(document).ready(function() {
+
+	var calc_glass_name = 0;
+	var calc_glass_value = 0;
+	var calc_glass_thin = 2;
+
+	$('.calc__select').click(function(){
+		$(this).toggleClass("calc__select--open");
+		$(".calc__list").toggleClass("calc__list--open");
+	});
+
+	$('.calc__li').click(function(e){
+		var calc_select_text = $(this).text();
+		$('.calc__current').text( calc_select_text );
+	});
+
+
+	$('#calc_checkbox_1').click(function(e){
+		if ( $(this).is(':checked') ) {
+			$('#calc_section_1').removeClass("calc__section--hide");
+		} else {
+			$('#calc_section_1').addClass("calc__section--hide");
+			$('input[name=calc_obrabotka]').prop("checked", false);
+		}
+	});
+
+	$('#calc_checkbox_2').click(function(e){
+		if ( $(this).is(':checked') ) {
+			$('#calc_section_2').removeClass("calc__section--hide");
+		} else {
+			$('#calc_section_2').addClass("calc__section--hide");
+			$('#calc_hole_sz').val(3);
+			$('#calc_hole_amount').val(0);
+		}
+	});
+
+	$('#calc_checkbox_3').click(function(e){
+		if ( $(this).is(':checked') ) {
+			$('#calc_section_3').removeClass("calc__section--hide");
+		} else {
+			$('#calc_section_3').addClass("calc__section--hide");
+			$('#calc_hole_outer').val(0);
+			$('#calc_hole_inner').val(0);
+		}
+	});
+
+	$('#calc_checkbox_4').click(function(e){
+		if ( $(this).is(':checked') ) {
+			$('#calc_section_4').removeClass("calc__section--hide");
+		} else {
+			$('#calc_section_4').addClass("calc__section--hide");
+			$('input[name=calc_facet]').prop("checked", false);
+			$('#calc_facet_width').val(10);
+		}
+	});
+	
+	$('#calc_checkbox_5').click(function(e){
+		if ( $(this).is(':checked') ) {
+			$('#calc_section_5').removeClass("calc__section--hide");
+		} else {
+			$('#calc_section_5').addClass("calc__section--hide");
+		}
+	});
+
+
+
+
+	$('#calc_hole_sz').on("input", function(){
+		setTimeout(() => {
+			if( $(this).val() < 3 ) {
+				$(this).val( 3 );
+			}
+			if( $(this).val() > 90 ) {
+				$(this).val( 90 );
+			}
+		}, 300);
+	});
+
+	$('#calc_facet_width').on("input", function(){
+		setTimeout(() => {
+			if( $(this).val() < 10 ) {
+				$(this).val( 10 );
+			}
+			if( $(this).val() > 35 ) {
+				$(this).val( 35 );
+			}
+		}, 300);
+	});
+
+	$('#calc_glass_list').empty();
+	let glassList = document.querySelector('#calc_glass_list');
+
+	for ( let i = 0; i < glassArr.length; i++ ) {
+		let glassDiv = document.createElement('div');
+		glassDiv.innerHTML = glassArr[i].name;
+		glassDiv.className = "calc__li";
+		glassList.appendChild( glassDiv );
+	}
+
+    $('.calc__list').on("click",".calc__li",function(e){
+        // setTimeout(() => {
+        //     $('.calc__close').removeClass("calc__close--show"); 
+        // }, 100);
+        $(this).parent(".calc__list").prev(".calc__current").text( $(this).text() );
+		// $(this).parent(".calc__list").prev().prev(".calc__street").val( $(this).text() );
+		calc_glass_name = $(this).text();
+
+	});
+	
+	$("#calc_glass_list").on("click",".calc__li",function(e){ 
+		calc_glass_value = glassArr.find(n1 => n1.name === $(this).text() ).value; 
+
+		calc_glass_thin = glassArr.find(t1 => t1.name === $(this).text() ).thin; 
+	});
+
+	$('input').on("input", function(){
+		calculation();
+	});
+	$(".calc__list").on("click", ".calc__li", function(){
+		calculation();
+	});
+
+
+	function calculation() {
+
+		console.clear();
+		console.log( "––––––---------------------------------------------" );
+		console.log( "calc_glass_name: " + calc_glass_name);
+		console.log( "calc_glass_value: " + calc_glass_value);
+		console.log( "calc_glass_thin: " + calc_glass_thin);
+
+		var calc_form = $('input[name=calc_form]:checked').val();
+		console.log( "calc_form: " + calc_form);
+
+		// if ( $('#calc_zakalka').is(':checked') ) {
+		// 	var calc_zakalka = 1;
+		// } else {
+		// 	var calc_zakalka = 0;
+		// }
+
+		//$('#calc_zakalka').prop("checked", false);
+
+		//console.log( "calc_zakalka: " + calc_zakalka);
+
+		if ( (calc_glass_thin == "2") || (calc_glass_thin == "15") || (calc_glass_thin == "19") || (calc_glass_thin == "4+4") || (calc_glass_thin == "5+5") || (calc_glass_thin == "6+6") ) {
+			$('.calc__zakalka').addClass("calc__zakalka--hide");
+			$('#calc_zakalka').prop("checked", false);
+		} else {
+			$('.calc__zakalka').removeClass("calc__zakalka--hide");
+		}
+
+		// if ( calc_glass_thin == "2" ) {
+		// 	$('#calc_label_1, #calc_label_2, #calc_label_3, #calc_label_4, #calc_label_5, #calc_label_6').addClass("calc__label--hide");
+		// } else {
+		// 	$('#calc_label_1, #calc_label_2, #calc_label_3, #calc_label_4, #calc_label_5, #calc_label_6').removeClass("calc__label--hide");
+		// }
+
+		// if ( (calc_glass_thin == "0") || (calc_glass_thin == "2") ) {
+		// 	$('.calc__label--obrabotka').addClass("calc__label--hide");
+		// } else {
+		// 	$('.calc__label--obrabotka').removeClass("calc__label--hide");
+		// }
+
+		// if ( (calc_glass_thin == "15") || (calc_glass_thin == "19") || (calc_glass_thin == "4+4") || (calc_glass_thin == "5+5") || (calc_glass_thin == "6+6") ) {
+		// 	$('#calc_label_5').addClass("calc__label--hide");
+		// 	$('#calc_label_6').addClass("calc__label--hide");
+		// } else {
+		// 	$('#calc_label_5').removeClass("calc__label--hide");
+		// 	$('#calc_label_6').removeClass("calc__label--hide");
+		// }
+
+		var calc_width = $('#calc_width').val();
+		var calc_height = $('#calc_height').val();
+		var calc_square = (+calc_width * +calc_height) / 1000000;
+
+		console.log( "calc_square: " + calc_square);
+
+		
+		// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––-––––––––––––––––––––––––––––––
+
+
+		var calc_obrabotka = 0;
+
+		if ( $('input[name=calc_obrabotka]:checked').val() == 1 ) {
+			calc_obrabotka = obrabotka_1_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+			
+		}
+		if ( $('input[name=calc_obrabotka]:checked').val() == 2 ) {
+			calc_obrabotka = obrabotka_2_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+			
+		}
+		if ( $('input[name=calc_obrabotka]:checked').val() == 3 ) {
+			calc_obrabotka = obrabotka_3_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+			
+		}
+		if ( $('input[name=calc_obrabotka]:checked').val() == 4 ) {
+			calc_obrabotka = obrabotka_4_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+			
+		}
+		if ( $('input[name=calc_obrabotka]:checked').val() == 5 ) {
+			calc_obrabotka = obrabotka_5_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+			
+		}
+		if ( $('input[name=calc_obrabotka]:checked').val() == 6 ) {
+			calc_obrabotka = obrabotka_6_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+			
+		}
+		if ( $('#calc_zakalka').is(':checked') ) {
+			var calc_zakalka = zakalka_Arr.find(z1 => z1.thin == calc_glass_thin ).value;
+		} else {
+			var calc_zakalka = 0
+		}
+
+		console.log("calc_obrabotka: " + calc_obrabotka);
+		console.log("calc_zakalka: " + calc_zakalka);
+
+
+		// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––-––––––––––––––––––––––––––––––
+
+
+		var calc_hole_sz = $('#calc_hole_sz').val();
+		var calc_hole_amount = $('#calc_hole_amount').val();
+		var calc_outer_amount = $('#calc_hole_outer').val();
+		var calc_inner_amount = $('#calc_hole_inner').val();
+
+
+		if ( calc_hole_sz > 0 ) {
+			if ( (calc_glass_thin == "4+4") || (calc_glass_thin == "5+5") || (calc_glass_thin == "6+6") ) {
+				var calc_hole_price = hole_1_Arr.find(h1 => h1.thin == calc_glass_thin ).value * 2;
+			} else {
+				var calc_hole_price = hole_1_Arr.find(h1 => h1.thin == calc_glass_thin ).value;
+			}
+		}
+		if ( calc_hole_sz > 12 ) {
+			if ( (calc_glass_thin == "4+4") || (calc_glass_thin == "5+5") || (calc_glass_thin == "6+6") ) {
+				var calc_hole_price = hole_2_Arr.find(h1 => h1.thin == calc_glass_thin ).value * 2;
+			} else {
+				var calc_hole_price = hole_2_Arr.find(h1 => h1.thin == calc_glass_thin ).value;
+			}
+		}
+		if ( calc_hole_sz > 30 ) {
+			if ( (calc_glass_thin == "4+4") || (calc_glass_thin == "5+5") || (calc_glass_thin == "6+6") ) {
+				var calc_hole_price = hole_3_Arr.find(h1 => h1.thin == calc_glass_thin ).value * 2;
+			} else {
+				var calc_hole_price = hole_3_Arr.find(h1 => h1.thin == calc_glass_thin ).value;
+			}
+			
+			
+		}
+		if ( calc_hole_sz > 70 ) {
+			if ( (calc_glass_thin == "4+4") || (calc_glass_thin == "5+5") || (calc_glass_thin == "6+6") ) {
+				var calc_hole_price = hole_4_Arr.find(h1 => h1.thin == calc_glass_thin ).value * 2;
+			} else {
+				var calc_hole_price = hole_4_Arr.find(h1 => h1.thin == calc_glass_thin ).value;
+			}
+		}
+
+
+		console.log("calc_hole_price: " + calc_hole_price);
+
+
+		if ( calc_outer_amount > 0 ) {
+			if ( (calc_glass_thin == "4+4") || (calc_glass_thin == "5+5") || (calc_glass_thin == "6+6") ) {
+				var calc_outer_price = calc_outer_Arr.find(x1 => x1.thin == calc_glass_thin ).value * 2;
+			} else {
+				var calc_outer_price = calc_outer_Arr.find(x1 => x1.thin == calc_glass_thin ).value;
+			}
+		} else {
+			var calc_outer_price = 0;
+		}
+
+		if ( calc_inner_amount > 0 ) {
+			if ( (calc_glass_thin == "4+4") || (calc_glass_thin == "5+5") || (calc_glass_thin == "6+6") ) {
+				var calc_inner_price = calc_inner_Arr.find(v1 => v1.thin == calc_glass_thin ).value * 2;
+			} else {
+				var calc_inner_price = calc_inner_Arr.find(v1 => v1.thin == calc_glass_thin ).value;
+			}
+		} else {
+			var calc_inner_price = 0;
+		}
+
+		console.log("calc_outer_price: " + calc_outer_price);
+		console.log("calc_outer_summ: " + ( calc_outer_amount * calc_outer_price ));
+		
+		console.log("calc_inner_price: " + calc_inner_price);
+		console.log("calc_inner_summ: " + ( calc_inner_amount * calc_inner_price ));
+		
+
+
+		// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––-––––––––––––––––––––––––––––––
+
+
+		var calc_facet = $('input[name=calc_facet]:checked').val();
+		var calc_facet_width = $('#calc_facet_width').val();
+		var calc_facet_price = 0;
+
+		if ( calc_facet == 1 ) {
+			if ( calc_facet_width > 0 )  { calc_facet_price = 260 }
+			if ( calc_facet_width > 15 ) { calc_facet_price = 320 }
+			if ( calc_facet_width > 20 ) { calc_facet_price = 440 }
+			if ( calc_facet_width > 30 ) { calc_facet_price = 600 }
+		}
+		if ( calc_facet == 2 ) {
+			if ( calc_facet_width > 0 )  { calc_facet_price = 660 }
+			if ( calc_facet_width > 15 ) { calc_facet_price = 840 }
+			if ( calc_facet_width > 20 ) { calc_facet_price = 1120 }
+			if ( calc_facet_width > 30 ) { calc_facet_price = 1400 }
+		}
+		
+		console.log("calc_facet_price: " + calc_facet_price);
+
+
+
+		if ( calc_square >= 4 ) {
+			var calc_facet_itog = calc_facet_price * 1.6 * calc_square;
+		} else {
+			if ( (calc_width >= 3000) || (calc_height > 3000) ) {
+				var calc_facet_itog = calc_facet_price * 1.4 * calc_square;
+			} else {
+				if ( (calc_width >= 2500) || (calc_height > 2500) ) {
+					var calc_facet_itog = calc_facet_price * 1.3 * calc_square;
+				} else {
+					var calc_facet_itog = calc_facet_price * calc_square;
+				}
+			}
+		}
+
+		if ( $('#calc_checkbox_4').is(':checked') ) {
+			if ( calc_facet_itog < 400 ) {
+				calc_facet_itog = 400;
+			}
+		}
+
+		console.log("calc_facet_itog: " + calc_facet_itog);
+
+
+		// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––-––––––––––––––––––––––––––––––
+
+
+		var calc_plenka = $('#calc_plenka').val();
+		var calc_plenka_itog = calc_plenka * 450;
+
+		console.log("calc_plenka_itog: "+ calc_plenka_itog);
+
+
+		// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––-––––––––––––––––––––––––––––––
+
+
+		var calc_quantity = $('#calc_quantity').val();
+
+		var calc_itog = +calc_quantity * +calc_square * ( +calc_glass_value + +calc_obrabotka + +calc_zakalka ) + ( +calc_hole_amount * +calc_hole_price ) + (+calc_outer_amount * +calc_outer_price) + (+calc_inner_amount * +calc_inner_price) + +calc_plenka_itog + calc_facet_itog;
+		$('#calc_itog').text( calc_itog.toFixed(2) + " руб." );
+
+	}
+
+
+
+});
