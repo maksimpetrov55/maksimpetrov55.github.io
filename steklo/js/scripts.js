3b7c7f4e1459cd22637609f8964893bf97a9bcde
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2020-09-03 17:11:00 +0600

    steklo
    03.09.2020
    17.10

--- steklo/js/scripts.js
+++ steklo/js/scripts.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 
 	var calc_glass_name = 0;
+	var calc_glass_name = 0;
 	var calc_glass_value = 0;
+	var calc_glass_type = 0;
 	var calc_glass_thin = 2;
+
 
 	$('.calc__select').click(function(){
 		$(this).toggleClass("calc__select--open");
@@ -50,15 +53,23 @@ $(document).ready(function() {
 		} else {
 			$('#calc_section_4').addClass("calc__section--hide");
 			$('input[name=calc_facet]').prop("checked", false);
-			$('#calc_facet_width').val(10);
+			//$('#calc_facet_width').val(10);
 		}
 	});
 	
-	$('#calc_checkbox_5').click(function(e){
+	// $('#calc_checkbox_5').click(function(e){
+	// 	if ( $(this).is(':checked') ) {
+	// 		$('#calc_section_5').removeClass("calc__section--hide");
+	// 	} else {
+	// 		$('#calc_section_5').addClass("calc__section--hide");
+	// 	}
+	// });
+
+	$('#calc_checkbox_6').click(function(e){
 		if ( $(this).is(':checked') ) {
-			$('#calc_section_5').removeClass("calc__section--hide");
+			$('#calc_section_6').removeClass("calc__section--hide");
 		} else {
-			$('#calc_section_5').addClass("calc__section--hide");
+			$('#calc_section_6').addClass("calc__section--hide");
 		}
 	});
 
@@ -109,7 +120,7 @@ $(document).ready(function() {
 	
 	$("#calc_glass_list").on("click",".calc__li",function(e){ 
 		calc_glass_value = glassArr.find(n1 => n1.name === $(this).text() ).value; 
-
+		calc_glass_type = glassArr.find(g1 => g1.name === $(this).text() ).type; 
 		calc_glass_thin = glassArr.find(t1 => t1.name === $(this).text() ).thin; 
 	});
 
@@ -129,7 +140,8 @@ $(document).ready(function() {
 		console.log( "calc_glass_value: " + calc_glass_value);
 		console.log( "calc_glass_thin: " + calc_glass_thin);
 
-		var calc_form = $('input[name=calc_form]:checked').val();
+		var calc_form = 0;
+		calc_form = $('input[name=calc_form]:checked').val();
 		console.log( "calc_form: " + calc_form);
 
 		// if ( $('#calc_zakalka').is(':checked') ) {
@@ -146,8 +158,22 @@ $(document).ready(function() {
 			$('.calc__zakalka').addClass("calc__zakalka--hide");
 			$('#calc_zakalka').prop("checked", false);
 		} else {
-			$('.calc__zakalka').removeClass("calc__zakalka--hide");
+			if	(
+				( calc_glass_type == "Зеркало серебро") ||
+				( calc_glass_type == "Зеркало бронза") ||
+				( calc_glass_type == "Зеркало графит") ||
+				( calc_glass_type == "Зеркало золото") ||
+				( calc_glass_type == "Зеркало осветленное") ||
+				( calc_glass_type == "Зеркало состаренное") 
+				) {
+					$('.calc__zakalka').addClass("calc__zakalka--hide");
+					$('#calc_zakalka').prop("checked", false);
+				} else {
+					$('.calc__zakalka').removeClass("calc__zakalka--hide");
+				}
 		}
+
+
 
 		// if ( calc_glass_thin == "2" ) {
 		// 	$('#calc_label_1, #calc_label_2, #calc_label_3, #calc_label_4, #calc_label_5, #calc_label_6').addClass("calc__label--hide");
@@ -171,6 +197,15 @@ $(document).ready(function() {
 
 		var calc_width = $('#calc_width').val();
 		var calc_height = $('#calc_height').val();
+
+		if ( calc_form != 1 ) {
+			calc_width = +$('#calc_width').val() + 100;
+			calc_height = +$('#calc_height').val() + 100;
+		}
+
+		console.log("calc_width: " + calc_width);
+		console.log("calc_height: " + calc_height);
+
 		var calc_square = (+calc_width * +calc_height) / 1000000;
 
 		console.log( "calc_square: " + calc_square);
@@ -181,30 +216,47 @@ $(document).ready(function() {
 
 		var calc_obrabotka = 0;
 
-		if ( $('input[name=calc_obrabotka]:checked').val() == 1 ) {
-			calc_obrabotka = obrabotka_1_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
-			
+
+		if ( calc_form <= 6 ) {
+			if ( $('input[name=calc_obrabotka]:checked').val() == 2 ) {
+				calc_obrabotka = obrabotka_3_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+			}
+			if ( $('input[name=calc_obrabotka]:checked').val() == 3 ) {
+				calc_obrabotka = obrabotka_1_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+			}
+		} else {
+			if ( $('input[name=calc_obrabotka]:checked').val() == 2 ) {
+				calc_obrabotka = obrabotka_4_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+			}
+			if ( $('input[name=calc_obrabotka]:checked').val() == 3 ) {
+				calc_obrabotka = obrabotka_2_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+			}
 		}
-		if ( $('input[name=calc_obrabotka]:checked').val() == 2 ) {
-			calc_obrabotka = obrabotka_2_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+		
+
+
+		// if ( $('input[name=calc_obrabotka]:checked').val() == 1 ) {
+		// 	calc_obrabotka = obrabotka_1_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+		// }
+		// if ( $('input[name=calc_obrabotka]:checked').val() == 2 ) {
+		// 	calc_obrabotka = obrabotka_2_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+		// }
+		// if ( $('input[name=calc_obrabotka]:checked').val() == 3 ) {
+		// 	calc_obrabotka = obrabotka_3_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+		// }
+		// if ( $('input[name=calc_obrabotka]:checked').val() == 4 ) {
+		// 	calc_obrabotka = obrabotka_4_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+		// }
+
+		// if ( $('input[name=calc_obrabotka]:checked').val() == 5 ) {
+		// 	calc_obrabotka = obrabotka_5_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
 			
-		}
-		if ( $('input[name=calc_obrabotka]:checked').val() == 3 ) {
-			calc_obrabotka = obrabotka_3_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
+		// }
+		// if ( $('input[name=calc_obrabotka]:checked').val() == 6 ) {
+		// 	calc_obrabotka = obrabotka_6_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
 			
-		}
-		if ( $('input[name=calc_obrabotka]:checked').val() == 4 ) {
-			calc_obrabotka = obrabotka_4_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
-			
-		}
-		if ( $('input[name=calc_obrabotka]:checked').val() == 5 ) {
-			calc_obrabotka = obrabotka_5_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
-			
-		}
-		if ( $('input[name=calc_obrabotka]:checked').val() == 6 ) {
-			calc_obrabotka = obrabotka_6_Arr.find(o1 => o1.thin == calc_glass_thin ).value;
-			
-		}
+		// }
+
 		if ( $('#calc_zakalka').is(':checked') ) {
 			var calc_zakalka = zakalka_Arr.find(z1 => z1.thin == calc_glass_thin ).value;
 		} else {
@@ -290,22 +342,43 @@ $(document).ready(function() {
 		// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––-––––––––––––––––––––––––––––––
 
 
+		if ( calc_form == 0 || calc_form == 7 || calc_form == 8 || calc_form == 15 ) {
+			$('#calc_facet_wrap').addClass("calc__section--hide");
+			$('input[name=calc_section_4]').prop("checked", false);
+			$('input[name=calc_facet]').prop("checked", false);
+		} else {
+			$('#calc_facet_wrap').removeClass("calc__section--hide");
+		}
+
+
 		var calc_facet = $('input[name=calc_facet]:checked').val();
-		var calc_facet_width = $('#calc_facet_width').val();
+		//var calc_facet_width = $('#calc_facet_width').val();
 		var calc_facet_price = 0;
 
-		if ( calc_facet == 1 ) {
-			if ( calc_facet_width > 0 )  { calc_facet_price = 260 }
-			if ( calc_facet_width > 15 ) { calc_facet_price = 320 }
-			if ( calc_facet_width > 20 ) { calc_facet_price = 440 }
-			if ( calc_facet_width > 30 ) { calc_facet_price = 600 }
+		if ( calc_form <= 6 ){
+			if ( calc_facet == 1 ) { calc_facet_price = 260 }
+			if ( calc_facet == 2 ) { calc_facet_price = 320 }
+			if ( calc_facet == 3 ) { calc_facet_price = 440 }
+			if ( calc_facet == 4 ) { calc_facet_price = 600 }
+		} else {
+			if ( calc_facet == 1 ) { calc_facet_price = 660  }
+			if ( calc_facet == 2 ) { calc_facet_price = 840  }
+			if ( calc_facet == 3 ) { calc_facet_price = 1120 }
+			if ( calc_facet == 4 ) { calc_facet_price = 1400 }
 		}
-		if ( calc_facet == 2 ) {
-			if ( calc_facet_width > 0 )  { calc_facet_price = 660 }
-			if ( calc_facet_width > 15 ) { calc_facet_price = 840 }
-			if ( calc_facet_width > 20 ) { calc_facet_price = 1120 }
-			if ( calc_facet_width > 30 ) { calc_facet_price = 1400 }
-		}
+
+		// if ( calc_facet == 1 ) {
+		// 	if ( calc_facet_width > 0 )  { calc_facet_price = 260 }
+		// 	if ( calc_facet_width > 15 ) { calc_facet_price = 320 }
+		// 	if ( calc_facet_width > 20 ) { calc_facet_price = 440 }
+		// 	if ( calc_facet_width > 30 ) { calc_facet_price = 600 }
+		// }
+		// if ( calc_facet == 2 ) {
+		// 	if ( calc_facet_width > 0 )  { calc_facet_price = 660 }
+		// 	if ( calc_facet_width > 15 ) { calc_facet_price = 840 }
+		// 	if ( calc_facet_width > 20 ) { calc_facet_price = 1120 }
+		// 	if ( calc_facet_width > 30 ) { calc_facet_price = 1400 }
+		// }
 		
 		console.log("calc_facet_price: " + calc_facet_price);
 
@@ -337,8 +410,30 @@ $(document).ready(function() {
 		// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––-––––––––––––––––––––––––––––––
 
 
-		var calc_plenka = $('#calc_plenka').val();
-		var calc_plenka_itog = calc_plenka * 450;
+		if	(
+			( calc_glass_type == "Зеркало серебро") ||
+			( calc_glass_type == "Зеркало бронза") ||
+			( calc_glass_type == "Зеркало графит") ||
+			( calc_glass_type == "Зеркало золото") ||
+			( calc_glass_type == "Зеркало осветленное") ||
+			( calc_glass_type == "Зеркало состаренное") 
+			) 
+		{
+			$('#calc_plenka_wrap').removeClass("calc__section--hide");
+		} else {
+			$('#calc_plenka_wrap').addClass("calc__section--hide");
+			$('input[name=calc_section_5]').prop("checked", false);
+		}
+
+
+		//var calc_plenka = $('#calc_plenka').val();
+
+		if ( $('#calc_checkbox_5').is(':checked') ) {
+			var calc_plenka_itog = calc_square * 450;
+		} else {
+			var calc_plenka_itog = 0;
+		}
+		
 
 		console.log("calc_plenka_itog: "+ calc_plenka_itog);
 
@@ -346,9 +441,44 @@ $(document).ready(function() {
 		// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––-––––––––––––––––––––––––––––––
 
 
+		if	(
+			( calc_glass_type == "Зеркало серебро") ||
+			( calc_glass_type == "Зеркало бронза") ||
+			( calc_glass_type == "Зеркало графит") ||
+			( calc_glass_type == "Зеркало золото") ||
+			( calc_glass_type == "Зеркало осветленное") ||
+			( calc_glass_type == "Зеркало состаренное") 
+			) 
+		{
+			$('#calc_kraska_wrap').addClass("calc__section--hide");
+			$('input[name=calc_section_6]').prop("checked", false);
+		} else {
+			$('#calc_kraska_wrap').removeClass("calc__section--hide");
+		}
+
+		if ( $('#calc_checkbox_6').is(':checked') ) {
+			var calc_kraska_itog = calc_square * 4250;
+			if ( calc_kraska_itog < 1000 ) {
+				calc_kraska_itog = 1000;
+			}
+		} else {
+			var calc_kraska_itog = 0;
+		}
+
+
+
+		// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––-––––––––––––––––––––––––––––––
+
+
+
 		var calc_quantity = $('#calc_quantity').val();
 
-		var calc_itog = +calc_quantity * +calc_square * ( +calc_glass_value + +calc_obrabotka + +calc_zakalka ) + ( +calc_hole_amount * +calc_hole_price ) + (+calc_outer_amount * +calc_outer_price) + (+calc_inner_amount * +calc_inner_price) + +calc_plenka_itog + calc_facet_itog;
+		var calc_itog = +calc_quantity * +calc_square * ( +calc_glass_value + +calc_obrabotka + +calc_zakalka ) + ( +calc_hole_amount * +calc_hole_price ) + (+calc_outer_amount * +calc_outer_price) + (+calc_inner_amount * +calc_inner_price) + +calc_plenka_itog + +calc_facet_itog + +calc_kraska_itog;
+		
+		if ( calc_itog < 500 ) {
+			calc_itog = 500;
+		}
+		
 		$('#calc_itog').text( calc_itog.toFixed(2) + " руб." );
 
 	}
