0fe3a4feba5b6959d2e9b7d47e7a6701f13c471d
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2020-12-09 21:49:19 +0600

    pechat
    09.12.2020
    21.49

--- works/pechat/js/scripts.js
+++ works/pechat/js/scripts.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
 			$('.calc__label--typeof').removeClass("calc__label--hide");
 			$('.calc__picture--show').removeClass("calc__picture--show");
 			$('#calc_image_1').addClass("calc__picture--show");
-			// $('#calc_label_material_1, #calc_label_material_2, #calc_label_material_3, #calc_label_material_4').removeClass("calc__label--hide");
-			//$('.calc__slider2').addClass("calc__slider2--hide");
+			$('#calc_label_material_1, #calc_label_material_2, #calc_label_material_3, #calc_label_material_4').removeClass("calc__label--hide");
+			$('.calc__slider2').addClass("calc__slider2--hide");
 		} else {
 			if ( calc_item == 2 ) {
 				$('.calc__line--typeof').removeClass("calc__line--hide");
@@ -18,8 +18,8 @@ $(document).ready(function() {
 				$('.calc__slider2').removeClass("calc__slider2--hide");
 				$('.calc__picture--show').removeClass("calc__picture--show");
 				$('#calc_image_2').addClass("calc__picture--show");
-				// $('#calc_label_material_1, #calc_label_material_2, #calc_label_material_3, #calc_label_material_4').removeClass("calc__label--hide");
-				// $('#calc_label_material_1, #calc_label_material_2, #calc_label_material_3').addClass("calc__label--hide");
+				$('#calc_label_material_1, #calc_label_material_2, #calc_label_material_3, #calc_label_material_4').removeClass("calc__label--hide");
+				//$('#calc_label_material_1, #calc_label_material_2, #calc_label_material_3').addClass("calc__label--hide");
 				$('#calc_material_4').prop("checked", true);
 			} else {
 				if ( calc_item == 3 ) {
@@ -28,15 +28,15 @@ $(document).ready(function() {
 					$('.calc__slider2').removeClass("calc__slider2--hide");
 					$('.calc__picture--show').removeClass("calc__picture--show");
 					$('#calc_image_3').addClass("calc__picture--show");
-					// $('#calc_label_material_1, #calc_label_material_2, #calc_label_material_3, #calc_label_material_4').removeClass("calc__label--hide");
+					$('#calc_label_material_1, #calc_label_material_2, #calc_label_material_3, #calc_label_material_4').removeClass("calc__label--hide");
 				} else {
 					$('.calc__line--typeof').addClass("calc__line--hide");
 					$('.calc__label--typeof').addClass("calc__label--hide");
 					$('.calc__slider2').removeClass("calc__slider2--hide");
 					$('.calc__picture--show').removeClass("calc__picture--show");
 					$('#calc_image_4').addClass("calc__picture--show");
-					// $('#calc_label_material_1, #calc_label_material_2, #calc_label_material_3, #calc_label_material_4').removeClass("calc__label--hide");
-					// $('#calc_label_material_1, #calc_label_material_2, #calc_label_material_3').addClass("calc__label--hide");
+					$('#calc_label_material_1, #calc_label_material_2, #calc_label_material_3, #calc_label_material_4').removeClass("calc__label--hide");
+					$('#calc_label_material_1, #calc_label_material_2, #calc_label_material_3').addClass("calc__label--hide");
 					$('#calc_material_4').prop("checked", true);
 				}
 			}
@@ -44,20 +44,74 @@ $(document).ready(function() {
 
 	});
 
+	calc_resize();
 
-	$("#polzunok1").slider({
-		animate: "slow",
-		orientation: "vertical",
-		range: "min",
-		value: 100,
-		step: 1,
-		min: 10,
-		max: 457,
-		slide : function(event, ui) {
-			$("#result-polzunok1").val(ui.value);
-			calculation();
-		}
+	$('input[name=calc_radio_form]').on("change", function(){
+		calc_resize();
 	});
+
+	function calc_resize(){
+
+		var calc_form = $('input[name=calc_radio_form]:checked').val();
+
+		if ( calc_form == 4 ) {
+			$("#polzunok1").slider({
+				animate: "slow",
+				orientation: "vertical",
+				range: "min",
+				value: 50,
+				step: 1,
+				min: 10,
+				max: 490,
+				slide : function(event, ui) {
+					$("#result-polzunok1").val(ui.value);
+					calculation();
+				}
+			});
+			$("#polzunok2").slider({
+				animate: "slow",
+				range: "min",
+				value: 50,
+				step: 1,
+				min: 10,
+				max: 320,
+				slide : function(event, ui) {
+					$("#result-polzunok2").val(ui.value);
+					calculation();
+				}
+			});
+		} else {
+			$("#polzunok1").slider({
+				animate: "slow",
+				orientation: "vertical",
+				range: "min",
+				value: 50,
+				step: 1,
+				min: 10,
+				max: 457,
+				slide : function(event, ui) {
+					$("#result-polzunok1").val(ui.value);
+					calculation();
+				}
+			});
+			$("#polzunok2").slider({
+				animate: "slow",
+				range: "min",
+				value: 50,
+				step: 1,
+				min: 10,
+				max: 302,
+				slide : function(event, ui) {
+					$("#result-polzunok2").val(ui.value);
+					calculation();
+				}
+			});
+		}
+
+	}
+
+
+
 	$( "#result-polzunok1" ).val($( "#polzunok1" ).slider( "value" ));
 	
 	$('#result-polzunok1').on("change", function(){
@@ -66,18 +120,6 @@ $(document).ready(function() {
 		calculation();
 	});
 
-	$("#polzunok2").slider({
-		animate: "slow",
-		range: "min",
-		value: 100,
-		step: 1,
-		min: 10,
-		max: 302,
-		slide : function(event, ui) {
-			$("#result-polzunok2").val(ui.value);
-			calculation();
-		}
-	});
 	$( "#result-polzunok2" ).val($( "#polzunok2" ).slider( "value" ));
 	
 	$('#result-polzunok2').on("change", function(){
@@ -85,6 +127,12 @@ $(document).ready(function() {
 		$( "#polzunok2" ).slider( "value", sliderInput );
 		calculation();
 	});
+	
+
+
+
+
+
 
 	$('.calc__input, .calc__slidervalue1, .calc__slidervalue2').on("change", function(){
 		calculation();
@@ -147,15 +195,25 @@ $(document).ready(function() {
 		console.log("calc_form " + calc_form);
 		console.log("calc_material " + calc_material);
 		console.log("calc_lamination " + calc_lamination);
+
+
 		
 		var calc_height = $( "#result-polzunok1" ).val();
 		var calc_width = $( "#result-polzunok2" ).val();
 
 
-		var calc_sz1 = Math.floor(457 / +calc_height);
-		var calc_sz2 = Math.floor(302 / +calc_width);
-		var calc_sz3 = Math.floor(302 / +calc_height);
-		var calc_sz4 = Math.floor(457 / +calc_width);
+		if ( calc_form == 4 ) {
+			var calc_sz1 = Math.floor(490 / +calc_height);
+			var calc_sz2 = Math.floor(320 / +calc_width);
+			var calc_sz3 = Math.floor(320 / +calc_height);
+			var calc_sz4 = Math.floor(490 / +calc_width);
+		} else {
+			var calc_sz1 = Math.floor(457 / +calc_height);
+			var calc_sz2 = Math.floor(302 / +calc_width);
+			var calc_sz3 = Math.floor(302 / +calc_height);
+			var calc_sz4 = Math.floor(457 / +calc_width);
+		}
+
 
 		var calc_merge1 = calc_sz1 * calc_sz2;
 		var calc_merge2 = calc_sz3 * calc_sz4;
@@ -177,7 +235,6 @@ $(document).ready(function() {
 		console.log(calc_shtuk_fin);
 
 
-
 		if ( calc_form == 3 ) {
 			var calc_x3 = 1.1;
 		} else {
@@ -193,77 +250,61 @@ $(document).ready(function() {
 		if ( calc_lamination == 1 ) {
 			var calc_x1 = 1;
 		} else {
-			var calc_x1 = 1.12;
+			if ( calc_form == 4 ) {
+				var calc_x1 = 1.2;
+			} else {
+				var calc_x1 = 1.15;
+			}
 		}
 
-		var calc_sale = 0.85;
+		if ( +calc_shtuk_fin >= 0 ) 	{ var calc_base = 0; 		var calc_set = 300;		var calc_rate = 20; }
+		if ( +calc_shtuk_fin >= 5 ) 	{ var calc_base = 5; 		var calc_set = 200;		var calc_rate = 10; }
+		if ( +calc_shtuk_fin >= 10 ) 	{ var calc_base = 10; 		var calc_set = 150;		var calc_rate = 2.9; }
+		if ( +calc_shtuk_fin >= 20 ) 	{ var calc_base = 20; 		var calc_set = 121;		var calc_rate = 0.829; }
+		if ( +calc_shtuk_fin >= 40 ) 	{ var calc_base = 40; 		var calc_set = 104.5;	var calc_rate = 0.325; }
+		if ( +calc_shtuk_fin >= 60 ) 	{ var calc_base = 60; 		var calc_set = 98;		var calc_rate = 0.325; }
+		if ( +calc_shtuk_fin >= 80 ) 	{ var calc_base = 80; 		var calc_set = 93.6;	var calc_rate = 0.219; }
+		if ( +calc_shtuk_fin >= 100 ) 	{ var calc_base = 100; 		var calc_set = 70;		var calc_rate = 0.0225; }
+		if ( +calc_shtuk_fin >= 260 ) 	{ var calc_base = 260; 		var calc_set = 66.5;	var calc_rate = 0.0095; }
+		if ( +calc_shtuk_fin >= 500 ) 	{ var calc_base = 500; 		var calc_set = 58.8;	var calc_rate = 0.0134; }
+		if ( +calc_shtuk_fin >= 1000 ) 	{ var calc_base = 1000; 	var calc_set = 47.1;	var calc_rate = 0.0015; }
+		if ( +calc_shtuk_fin >= 1700 ) 	{ var calc_base = 1700; 	var calc_set = 46.1;	var calc_rate = 0.00003; }
 
-		if ( +calc_shtuk_fin >= 0 ) {
-			var calc_base = 0;
-			var calc_set = 300;
-			var calc_rate = 20;
-		}
-		if ( +calc_shtuk_fin >= 5 ) {
-			var calc_base = 5;
-			var calc_set = 200;
-			var calc_rate = 10;
-		}
-		if ( +calc_shtuk_fin >= 10 ) {
-			var calc_base = 10;
-			var calc_set = 150;
-			var calc_rate = 2.9;
-		}
-		if ( +calc_shtuk_fin >= 20 ) {
-			var calc_base = 20;
-			var calc_set = 121;
-			var calc_rate = 0.829;
-		}
-		if ( +calc_shtuk_fin >= 40 ) {
-			var calc_base = 40;
-			var calc_set = 104.5;
-			var calc_rate = 0.325;
-		}
-		if ( +calc_shtuk_fin >= 60 ) {
-			var calc_base = 60;
-			var calc_set = 98;
-			var calc_rate = 0.325;
-		}
-		if ( +calc_shtuk_fin >= 80 ) {
-			var calc_base = 80;
-			var calc_set = 93.6;
-			var calc_rate = 0.219;
-		}
-		if ( +calc_shtuk_fin >= 100 ) {
-			var calc_base = 100;
-			var calc_set = 70;
-			var calc_rate = 0.0225;
-		}
-		if ( +calc_shtuk_fin >= 260 ) {
-			var calc_base = 260;
-			var calc_set = 66.5;
-			var calc_rate = 0.0095;
-		}
-		if ( +calc_shtuk_fin >= 500 ) {
-			var calc_base = 500;
-			var calc_set = 58.8;
-			var calc_rate = 0.0134;
-		}
-		if ( +calc_shtuk_fin >= 1000 ) {
-			var calc_base = 1000;
-			var calc_set = 47.1;
-			var calc_rate = 0.0015;
-		}
-		if ( +calc_shtuk_fin >= 1700 ) {
-			var calc_base = 1700;
-			var calc_set = 46.1;
-			var calc_rate = 0.00003;
-		}
+		if ( +calc_shtuk_fin >= 1 ) 	{ var calc_base4 = 1; 		var calc_set4 = 360.00; var calc_rate4 = 155.000; }
+		if ( +calc_shtuk_fin >= 2 ) 	{ var calc_base4 = 2; 		var calc_set4 = 205.00; var calc_rate4 = 51.6667; }
+		if ( +calc_shtuk_fin >= 3 ) 	{ var calc_base4 = 3; 		var calc_set4 = 153.33; var calc_rate4 = 25.8333; }
+		if ( +calc_shtuk_fin >= 4 ) 	{ var calc_base4 = 4; 		var calc_set4 = 127.50; var calc_rate4 = 15.5000; }
+		if ( +calc_shtuk_fin >= 5 ) 	{ var calc_base4 = 5; 		var calc_set4 = 112.00; var calc_rate4 = 6.40000; }
+		if ( +calc_shtuk_fin >= 10 ) 	{ var calc_base4 = 10; 		var calc_set4 = 80.00; var calc_rate4 = 1.85000; }
+		if ( +calc_shtuk_fin >= 20 ) 	{ var calc_base4 = 20; 		var calc_set4 = 61.50; var calc_rate4 = 0.78333; }
+		if ( +calc_shtuk_fin >= 30 ) 	{ var calc_base4 = 30; 		var calc_set4 = 53.67; var calc_rate4 = 0.36667; }
+		if ( +calc_shtuk_fin >= 40 ) 	{ var calc_base4 = 40; 		var calc_set4 = 50.00; var calc_rate4 = 0.25000; }
+		if ( +calc_shtuk_fin >= 60 ) 	{ var calc_base4 = 60; 		var calc_set4 = 45.00; var calc_rate4 = 0.17500; }
+		if ( +calc_shtuk_fin >= 80 ) 	{ var calc_base4 = 80; 		var calc_set4 = 41.50; var calc_rate4 = 0.15500; }
+		if ( +calc_shtuk_fin >= 100 ) 	{ var calc_base4 = 100;		var calc_set4 = 38.40; var calc_rate4 = 0.02267; }
+		if ( +calc_shtuk_fin >= 150 ) 	{ var calc_base4 = 150;		var calc_set4 = 37.27; var calc_rate4 = 0.01233; }
+		if ( +calc_shtuk_fin >= 200 ) 	{ var calc_base4 = 200;		var calc_set4 = 36.65; var calc_rate4 = 0.00817; }
+		if ( +calc_shtuk_fin >= 300 ) 	{ var calc_base4 = 300;		var calc_set4 = 35.83; var calc_rate4 = 0.00808; }
+		if ( +calc_shtuk_fin >= 400 ) 	{ var calc_base4 = 400;		var calc_set4 = 35.03; var calc_rate4 = 0.01265; }
+		if ( +calc_shtuk_fin >= 500 ) 	{ var calc_base4 = 500;		var calc_set4 = 33.76; var calc_rate4 = 0.01294; }
+		if ( +calc_shtuk_fin >= 1000 )	{ var calc_base4 = 1000; 	var calc_set4 = 27.29; var calc_rate4 = 0.00132; }
+		if ( +calc_shtuk_fin >= 2000 )	{ var calc_base4 = 2000; 	var calc_set4 = 25.97; var calc_rate4 = 0.00005; }
+		if ( +calc_shtuk_fin >= 4000 )	{ var calc_base4 = 4000; 	var calc_set4 = 25.87; var calc_rate4 = 0.00005; }
+
 
 		console.log("calc_base " + calc_base);
 		console.log("calc_set " + calc_set);
 		console.log("calc_rate " + calc_rate);
 
-		var calc_preresult = +calc_shtuk_fin * ( +calc_set - ( +calc_rate * ( +calc_shtuk_fin - +calc_base ) ) );
+		var calc_sale = 0.95; // Коэффициент скидки
+
+		if ( calc_form == 4 ) {
+			var calc_sale = 1; // Коэффициент скидки
+			var calc_preresult = +calc_shtuk_fin * ( +calc_set4 - ( +calc_rate4 * ( +calc_shtuk_fin - +calc_base4 ) ) );
+		} else {
+			var calc_sale = 0.95; // Коэффициент скидки
+			var calc_preresult = +calc_shtuk_fin * ( +calc_set - ( +calc_rate * ( +calc_shtuk_fin - +calc_base ) ) );
+		}
 
 		var calc_result = ( +calc_preresult * +calc_x1 * +calc_x2 * +calc_x3 * +calc_sale) + +calc_vid_sum;
 
@@ -275,52 +316,6 @@ $(document).ready(function() {
 			$('.calc__price').addClass("calc__price--margin");
 		}
 
-		// (calc_base * calc_set) + ( (+calc_shtuk_fin - calc_base) * (calc_set - (calc_rate * (+calc_shtuk_fin - calc_base)))
-
-
-
-		
-
-		// if ( +calc_shtuk_fin < 100 ) {
-		// 	var calc_base = 5880;
-		// 	var calc_rate = 0.272;
-		// 	var calc_one =  ( 98 - (+calc_shtuk_fin - 60) * +calc_rate);
-		// } else {
-		// 	if ( +calc_shtuk_fin < 260 ) {
-		// 		var calc_base = 7000;
-		// 		var calc_rate = 0.0225;
-		// 		var calc_one =  ( 70 - (+calc_shtuk_fin - 100) * +calc_rate);
-		// 	} else {
-		// 		if ( +calc_shtuk_fin < 500 ) {
-		// 			var calc_base = 7000;
-		// 			var calc_rate = 0.0095;
-		// 			var calc_one =  ( 66.5 - (+calc_shtuk_fin - 100) * +calc_rate);
-		// 		} else {
-		// 			if ( +calc_shtuk_fin < 1000 ) {
-		// 				var calc_base = 7000;
-		// 				var calc_rate = 0.0134;
-		// 				var calc_one =  ( 61 - (+calc_shtuk_fin - 100) * +calc_rate);
-		// 			} else {
-		// 				if ( +calc_shtuk_fin < 1700 ) {
-		// 					var calc_base = 7000;
-		// 					var calc_rate = 0.0011;
-		// 					var calc_one =  ( 46 - (+calc_shtuk_fin - 100) * +calc_rate);
-		// 				} else {
-		// 					var calc_base = 7000;
-		// 					var calc_rate = 0.00003;
-		// 					var calc_one =  ( 44.5 - (+calc_shtuk_fin - 100) * +calc_rate);
-		// 				}
-		// 			}
-		// 		}
-				
-		// 	}
-		// }
-
-
-
-
-		//var calc_result = ((+calc_base + ( +calc_shtuk_fin - 100 ) * +calc_one) * +calc_x1 * +calc_x2 * +calc_x3 * +calc_sale) + +calc_vid_sum;
-		//console.log(calc_one);
 
 		$('#calc_itog').val( calc_result.toFixed(0) + " руб" );
 
@@ -348,45 +343,6 @@ $(document).ready(function() {
 		// 	var calc_k3 = 1;
 		// }
 
-		// if ( calc_material == 4 ) {
-		// 	var calc_k1 = 1;
-		// 	var calc_k4 = 0.45;
-
-		// 	if ( calc_shtuk < 1000 ) { var calc_k5 = 0.82 } else {
-		// 		if ( calc_shtuk < 2000 ) { var calc_k5 = 0.72 } else {
-		// 			if ( calc_shtuk < 3000 ) { var calc_k5 = 0.62 } else {
-		// 				if ( calc_shtuk < 4000 ) { var calc_k5 = 0.6 } else {
-		// 					if ( calc_shtuk < 5000 ) { var calc_k5 = 0.58 } else {
-		// 						if ( calc_shtuk < 6000 ) { var calc_k5 = 0.54 } else {
-		// 							if ( calc_shtuk < 7000 ) { var calc_k5 = 0.5 } else {
-		// 								if ( calc_shtuk < 8000 ) { var calc_k5 = 0.48 } else {
-		// 									if ( calc_shtuk < 9000 ) { var calc_k5 = 0.46 } else {
-		// 										if ( calc_shtuk < 10000 ) { var calc_k5 = 0.45 } else {
-		// 											if ( calc_shtuk < 11000 ) { var calc_k5 = 0.44 } else {
-		// 												if ( calc_shtuk < 13000 ) { var calc_k5 = 0.43 } else {
-		// 													if ( calc_shtuk < 15000 ) { var calc_k5 = 0.42 } else {
-		// 														if ( calc_shtuk < 20000 ) { var calc_k5 = 0.41 } else {
-		// 															var calc_k5 = 0.40;
-		// 														}
-		// 													}
-		// 												}
-		// 											}
-		// 										}
-		// 									}
-		// 								}
-		// 							}
-		// 						}
-		// 					}
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-
-		// } else {
-		// 	var calc_k1 = 1.4;
-		// 	var calc_k4 = 1;
-		// 	var calc_k5 = 1;
-		// }
 		
 		// if ( calc_lamination == 1 ) {
 		// 	var calc_k2 = 1;
