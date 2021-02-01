f0eb2e7cf4047d40be4e29b5f45cebb86c6c980e
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2021-02-01 21:40:54 +0600

    almarta
    01.02.2021
    21.40

---
+++ works/almarta/js/scripts.js
@@ -0,0 +1,122 @@
+$(document).ready(function() {
+
+	// $('.quiz__btn').click(function(){
+	// 	calculate();
+	// });
+
+	// function calculate() {
+
+	// 	var color = ".color1";
+	// 	var size = ".size1";
+
+	// 	var selector = color + " " + size;
+	// 	var length = $('.quiz__elements').children(".color1, .size1").length;
+		
+	// 	console.log(length);
+
+	// 	$('.quiz__elements').children(".color3.size1").addClass("quiz__item--select");
+
+	// }
+
+	$('.quiz__radio').on("change", function(){
+		quiz();
+	});
+
+	function quiz() {
+
+		$('.quiz__item--select').removeClass("quiz__item--select");
+
+		var radio_1 = $('input[name=quiz_kronshtein]:checked').val();
+		var radio_2 = $('input[name=quiz_karbonat]:checked').val();
+		var radio_3 = $('input[name=quiz_kozirek]:checked').val();
+		var radio_4 = $('input[name=quiz_section]:checked').val();
+
+		console.log( radio_1 + " | " + radio_2 + " | " + radio_3  + " | " + radio_4 );
+		
+		if ( radio_1 != undefined && radio_2 != undefined && radio_3 != undefined && radio_4 != undefined ) {
+			
+			var selector = "." + radio_1 + "." + radio_2 + "." + radio_3 + "." + radio_4;
+			
+			//console.log(selector);
+			
+			console.log( $(selector).length );
+
+			if ( $(selector).length == 0 || $(selector).length == undefined ) {
+				$('.quiz__sorry').addClass("quiz__sorry--show");
+			} else {
+				$('.quiz__sorry--show').removeClass("quiz__sorry--show");
+			}
+			
+			$('.quiz__elements').children( selector ).addClass("quiz__item--select");
+			
+
+		}
+
+	}
+
+	$('#quiz_next_1').click(function(){
+		var radio_1 = $('input[name=quiz_kronshtein]:checked').val();
+		if ( radio_1 != undefined ) {
+			$('#quiz_step_1').removeClass("quiz__step--show");
+			$('#quiz_step_2').addClass("quiz__step--show");
+			$(this).parent().prev().removeClass("quiz__error--show");
+		} else {
+			$(this).parent().prev().addClass("quiz__error--show");
+		}
+
+	});
+	$('#quiz_next_2').click(function(){
+		var radio_2 = $('input[name=quiz_karbonat]:checked').val();
+		if ( radio_2 != undefined ) {
+			$('#quiz_step_2').removeClass("quiz__step--show");
+			$('#quiz_step_3').addClass("quiz__step--show");
+			$(this).parent().prev().removeClass("quiz__error--show");
+		} else {
+			$(this).parent().prev().addClass("quiz__error--show");
+		}
+
+	});
+	$('#quiz_next_3').click(function(){
+		var radio_3 = $('input[name=quiz_kozirek]:checked').val();
+		if ( radio_3 != undefined ) {
+			$('#quiz_step_3').removeClass("quiz__step--show");
+			$('#quiz_step_4').addClass("quiz__step--show");
+			$(this).parent().prev().removeClass("quiz__error--show");
+		} else {
+			$(this).parent().prev().addClass("quiz__error--show");
+		}
+
+	});
+	$('#quiz_next_4').click(function(){
+		var radio_4 = $('input[name=quiz_section]:checked').val();
+		if ( radio_4 != undefined ) {
+			$('#quiz_step_4').removeClass("quiz__step--show");
+			$('#quiz_itog').addClass("quiz__itog--show");
+			$(this).parent().prev().removeClass("quiz__error--show");
+		} else {
+			$(this).parent().prev().addClass("quiz__error--show");
+		}
+
+	});
+
+
+	$('#quiz_prev_2').click(function(){
+		$('#quiz_step_2').removeClass("quiz__step--show");
+		$('#quiz_step_1').addClass("quiz__step--show");
+	});
+	$('#quiz_prev_3').click(function(){
+		$('#quiz_step_3').removeClass("quiz__step--show");
+		$('#quiz_step_2').addClass("quiz__step--show");
+	});
+	$('#quiz_prev_4').click(function(){
+		$('#quiz_step_4').removeClass("quiz__step--show");
+		$('#quiz_step_3').addClass("quiz__step--show");
+	});
+
+
+	$('.quiz__again').click(function(){
+		$('#quiz_itog').removeClass("quiz__itog--show");
+		$('#quiz_step_1').addClass("quiz__step--show");
+	});
+
+});
