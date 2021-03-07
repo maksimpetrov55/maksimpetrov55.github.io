330601173fb47dbda6099247b9aca35bbb250394
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2021-03-08 01:00:00 +0600

    garden
    08.03.2021
    0.59

---
+++ works/garden/!mail.php
@@ -0,0 +1,52 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['form_name'];
+$phone = $_POST['form_phone'];
+$capcha = $_POST['form_capcha'];
+$message = $_POST['form_msg'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'mailpost1231@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'a21awewq2e121e'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('mailpost1231@yandex.ru'); // от кого будет уходить письмо?
+$mail->addAddress('maksimpetrov55@yandex.ru');     // Кому будет уходить письмо
+$mail->addAddress('toplandscape@yandex.ru');
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    for ($ct = 0; $ct < count($_FILES['userfile']['tmp_name']); $ct++) {
+        $uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES['userfile']['name'][$ct]));
+        $filename = $_FILES['userfile']['name'][$ct];
+        if (move_uploaded_file($_FILES['userfile']['tmp_name'][$ct], $uploadfile)) {
+            $mail->addAttachment($uploadfile, $filename);
+        } else {
+            $msg .= 'Failed to move file to ' . $uploadfile;
+        }
+    }
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с сайта ГОРОД-САД';
+$mail->Body    = '<br>Имя: ' .$name. '<br>Телефон: ' .$phone. '<br>Сообщение: ' .$message. '<br>Капча:' .$capcha. '<br>' ;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: index.html#tnks');
+    //header('location: index.html');
+}
+?>
---
+++ works/garden/js/scripts.js
@@ -0,0 +1,216 @@
+$(document).ready(function() {
+
+    //плавный скролл до якоря
+    $(function(){
+        $("a[href^='#']").click(function(){
+            var _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+        });
+    });
+    //плавный скролл до якоря
+
+    //открытие-закрытие формы
+    $(".form").fadeOut(0);
+    $(".form").css({transform: "translateY(0px)"})
+
+    function form_open(){
+        $(".form").fadeIn(300);
+        $(".form__content").addClass("form__content--show");
+    }
+    function form_close(){
+        $(".form").fadeOut(400);
+        $(".form__content").removeClass("form__content--show");
+    }
+    $(".form__close").click(function(){
+        form_close();
+        $(".form__content__title").html("Оставить заявку");
+    });
+
+
+    $(".form-callback").click(function(){
+        $(".form__content__title").html("Заказать звонок");
+        form_open();
+    });
+    $(".form-order").click(function(){
+        $(".form__content__title").html("Оставить заявку");
+        form_open();
+    });
+    //открытие-закрытие формы
+
+    //отправка формы
+    $('input[name=form_capcha]').val( 100500 );
+    $('.capcha__radio').change(function(){
+        var capchaValue = $('input[name=capcha_radio]:checked').val();
+        $('input[name=form_capcha]').val(capchaValue);
+
+        if ( capchaValue == 0 ) {
+            $('.capcha-stop').addClass("capcha-stop--hide");
+            $('.form__content__btn').removeClass("form__content__btn--hidden")
+        } else {
+            $('.capcha-stop').removeClass("capcha-stop--hide");
+            $('.form__content__btn').addClass("form__content__btn--hidden")
+        }
+    });
+
+    $(".form__content__btn").click(function(){
+        var capchaValue = 1;
+        capchaValue = $('input[name=capcha_radio]:checked').val();
+
+        if ( capchaValue == 0 ) {
+            $( "form" ).submit();
+        } else {
+            document.location.href = "http://www.yandex.ru";
+        }
+    });
+    //отправка формы
+
+    //появление заголовка
+    setTimeout(function(){
+        $(".first__title").animate({opacity: "1"}, 1000);
+    }, 800)
+    //появление заголовка
+
+    //появление кнопки
+    setTimeout(function(){
+        $(".first__btn").animate({opacity: "1"}, 200);
+    }, 1000)
+    //появление кнопки
+
+    //whatsapp кнопка
+    $(".wa-btn__link").mouseover(function(){
+        $(".wa-btn__note").addClass('wa-btn__note--animation');
+    });
+    $(".wa-btn__link").mouseleave(function(){
+        $(".wa-btn__note").removeClass('wa-btn__note--animation');
+    });
+    $(".wa-btn__link").click(function(){
+        $(".wa-btn__note").removeClass('wa-btn__note--animation');
+    });
+    //whatsapp кнопка
+
+    //info
+    function info() {
+        var info_width = $(window).width(); 
+        var info_width2 = window.innerWidth;
+        $(".info__width").html(info_width.toFixed(2));
+        $(".info__width2").html(info_width2.toFixed(2));
+    }
+
+    $(window).scroll(function(e){
+        info();
+    });
+
+    $(window).resize(function() {
+        info();
+    });
+    //info
+
+    //бургер меню
+    $(".header__burger").click(function(){
+        $(".header__menu").toggleClass("header__menu--show");
+    });
+    $(".header__link").click(function(){
+        $(".header__menu").removeClass("header__menu--show");
+    });
+    //бургер меню
+
+    //выбор города
+    $(".first__city__current, .first__city__select--omsk, .first__city__select--tumen, .first__city__select--tobol").click(function(){
+        $(".first__city__select").toggleClass("first__city__select--show");
+    });
+    $(".first__city__select--omsk").click(function(){
+        $(".first, .serv").fadeOut(50).delay(100).fadeIn(200);
+        $(".first__city__current").html("Омск")
+    });
+    $(".first__city__select--tumen").click(function(){
+        $(".first, .serv").fadeOut(50).delay(100).fadeIn(200);
+        $(".first__city__current").html("Тюмень")
+    });
+    $(".first__city__select--tobol").click(function(){
+        $(".first, .serv").fadeOut(50).delay(100).fadeIn(200);
+        $(".first__city__current").html("Тобольск")
+    });
+    //выбор города
+
+    //раскрытие услуги
+    // $(".serv__block").click(function(){
+    //     if( $(".serv__block").hasClass("serv__block--open") ) {
+    //         $(".serv__block").not($(this)).removeClass("serv__block--open");
+    //         $(".serv__block__text").not($(this).children(".serv__block__text")).slideUp(300);
+    //     }
+    //     $(this).toggleClass("serv__block--open");
+    //     $(this).children(".serv__block__text").slideToggle(300);
+    // });
+
+    // if( $(".serv__block").hasClass("serv__block--open") ) {
+    // }
+
+    $(".serv__block").hover(function(e){
+        $(this).toggleClass("serv__block--hover");
+        $(this).children(".serv__wrap").stop().slideToggle(450);
+        $(this).children(".serv__shadow").toggleClass("serv__shadow--hover");
+        $(this).children(".serv__block__title").toggleClass("serv__block__title--hover");
+    });
+    //раскрытие услуги
+
+    //спасибо за заявку
+    if ( window.location.hash == '#tnks' ) {
+        $(".tnks").removeClass("tnks--hide");
+    }
+    $(".tnks, .tnks__close").click(function(){
+        window.location = "index.html";
+    });
+    //спасибо за заявку
+
+    //включение slick slider
+    $('.single-item').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: true,
+      //dotsClass: 'dots',
+      prevArrow: "<div class='arrow prev'></div>",
+      nextArrow: "<div class='arrow next'></div>",
+    });
+    //включение slick slider
+
+    //смена шрифта
+    $(".font").click(function(){
+        font_change();
+    });
+    function font_change(){
+        var font_value = prompt();
+        //alert(font_value);
+        if (font_value == 1) {
+            $(".g-font").removeClass("g-font--1");
+            $(".g-font").removeClass("g-font--2");
+            $(".g-font").removeClass("g-font--3");
+            $(".g-font").addClass("g-font--1");
+        } else {
+            if (font_value == 2) {
+                $(".g-font").removeClass("g-font--1");
+                $(".g-font").removeClass("g-font--2");
+                $(".g-font").removeClass("g-font--3");
+                $(".g-font").addClass("g-font--2");
+            } else {
+                if (font_value == 3) {
+                    $(".g-font").removeClass("g-font--1");
+                    $(".g-font").removeClass("g-font--2");
+                    $(".g-font").removeClass("g-font--3");
+                    $(".g-font").addClass("g-font--3");
+                }
+            }
+        }
+
+    }
+    //смена шрифта
+
+    //маска ввода
+    $(function(){
+		$("#form_phone").mask("+7 (999) 999-99-99", {placeholder: "__________" });
+	});
+    //маска ввода
+
+});
+
