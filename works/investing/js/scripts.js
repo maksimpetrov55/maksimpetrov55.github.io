fdcfa7c033f919267b47dd586cc29345d985a5b0
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2021-09-01 20:27:45 +0600

    investing
    01.09.2021
    20.27

--- works/investing/js/scripts.js
+++ works/investing/js/scripts.js
@@ -345,45 +345,6 @@ $(document).ready(function() {
         $('#chart_4').empty();
         $('#chart_5').empty();
 
-        var t1_date_ARR = [];
-        var t1_svoi_ARR = [];
-        var t1_contract_ARR = [];
-        var t1_amount_ARR = [];
-        var t1_add_ARR = [];
-        var t1_end_ARR = [];
-        var t1_clear_ARR = [];
-
-        var t2_date_ARR = [];
-        var t2_svoi_ARR = [];
-        var t2_contract_ARR = [];
-		var t2_amount_ARR = [];
-        var t2_add_ARR = [];
-        var t2_end_ARR = [];
-        var t2_clear_ARR = [];
-
-        var t3_date_ARR = [];
-        var t3_svoi_ARR = [];
-        var t3_contract_ARR = [];
-        var t3_amount_ARR = [];
-        var t3_add_ARR = [];
-        var t3_end_ARR = [];
-        var t3_clear_ARR = [];
-
-        var t4_date_ARR = [];
-        var t4_svoi_ARR = [];
-        var t4_contract_ARR = [];
-		var t4_amount_ARR = [];
-        var t4_add_ARR = [];
-        var t4_end_ARR = [];
-        var t4_clear_ARR = [];
-
-        var t5_date_ARR = [];
-        var t5_svoi_ARR = [];
-        var t5_contract_ARR = [];
-        var t5_amount_ARR = [];
-        var t5_add_ARR = [];
-        var t5_end_ARR = [];
-        var t5_clear_ARR = [];
 
         var date = new Date();
         var t1_date_month = date.getMonth();
