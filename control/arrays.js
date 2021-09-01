875b4170edb9c87618d94df2c9bace817e1d42a1
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2021-09-01 20:22:50 +0600

    control
    01.09.2021
    20.22

---
+++ control/arrays.js
@@ -0,0 +1,40 @@
+
+var t1_date_ARR = [];
+var t1_svoi_ARR = [];
+var t1_contract_ARR = [];
+var t1_amount_ARR = [];
+var t1_add_ARR = [];
+var t1_end_ARR = [];
+var t1_clear_ARR = [];
+
+var t2_date_ARR = [];
+var t2_svoi_ARR = [];
+var t2_contract_ARR = [];
+var t2_amount_ARR = [];
+var t2_add_ARR = [];
+var t2_end_ARR = [];
+var t2_clear_ARR = [];
+
+var t3_date_ARR = [];
+var t3_svoi_ARR = [];
+var t3_contract_ARR = [];
+var t3_amount_ARR = [];
+var t3_add_ARR = [];
+var t3_end_ARR = [];
+var t3_clear_ARR = [];
+
+var t4_date_ARR = [];
+var t4_svoi_ARR = [];
+var t4_contract_ARR = [];
+var t4_amount_ARR = [];
+var t4_add_ARR = [];
+var t4_end_ARR = [];
+var t4_clear_ARR = [];
+
+var t5_date_ARR = [];
+var t5_svoi_ARR = [];
+var t5_contract_ARR = [];
+var t5_amount_ARR = [];
+var t5_add_ARR = [];
+var t5_end_ARR = [];
+var t5_clear_ARR = [];
