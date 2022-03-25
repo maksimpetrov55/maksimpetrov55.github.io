8764e1ede12368618af2ac790bbf3caab4f4beee
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2022-03-26 03:01:46 +0600

    shengen
    26.03.2022
    3.01

--- works/shengen/js/scripts.js
+++ works/shengen/js/scripts.js
@@ -1,39 +1,84 @@
 $(document).ready(function() {
 
-	var today = new Date();
-	var time = new Date( today.getTime() + (24 * 3600 * 1000 * 2 ) );
+	// var today = new Date();
+	// var time = new Date( today.getTime() + (24 * 3600 * 1000 * 2 ) );
 
-	var timeD = time.getDate();
-	var timeM = time.getMonth() + 1;
-	var timeY = time.getFullYear();
+	// var timeD = time.getDate();
+	// var timeM = time.getMonth() + 1;
+	// var timeY = time.getFullYear();
 
-	if ( timeM < 10 ) {timeM = "0" + timeM;}
-	if ( timeD < 10 ) {timeD = "0" + timeD;}
+	// if ( timeM < 10 ) {timeM = "0" + timeM;}
+	// if ( timeD < 10 ) {timeD = "0" + timeD;}
 
-	$('#inp1').val( timeY + "-" + timeM + "-" + timeD );
+	// $('#inp1').val( timeY + "-" + timeM + "-" + timeD );
 
-	var inp1 = $('#inp1').val();
-	var date_check = new Date(inp1);
-	var date_90 = Math.ceil(Math.abs(date_check.getTime() - (24 * 3600 * 1000 * (90 + 1) ) ));
-	var date_180 = Math.ceil(Math.abs(date_check.getTime() - (24 * 3600 * 1000 * (180 + 1) ) ));
+	// var inp1 = $('#inp1').val();
+	// var date_check = new Date(inp1);
+	// var date_90 = Math.ceil(Math.abs(date_check.getTime() - (24 * 3600 * 1000 * (90 - 1) ) ));
+	// var date_180 = Math.ceil(Math.abs(date_check.getTime() - (24 * 3600 * 1000 * (180 - 1) ) ));
 
-	var date_90_value = new Date(date_90);
-	var date_180_value = new Date(date_180);
+	// var date_90_value = new Date(date_90);
+	// var date_180_value = new Date(date_180);
 
-	if ( inp1 != "" ){
-		$('.calc__date--90').text( date_90_value.getDate() + "." + (date_90_value.getMonth() + 1) + "." + date_90_value.getFullYear() );
-		$('.calc__date--180').text( date_180_value.getDate() + "." + (date_180_value.getMonth() + 1) + "." + date_180_value.getFullYear() );
-	}
+	// if ( inp1 != "" ){
+	// 	$('.calc__date--90').text( date_90_value.getDate() + "." + (date_90_value.getMonth() + 1) + "." + date_90_value.getFullYear() );
+	// 	$('.calc__date--180').text( date_180_value.getDate() + "." + (date_180_value.getMonth() + 1) + "." + date_180_value.getFullYear() );
+	// }
 
+	$('.calc__show').click(function(){
+		$(this).css({display: "none"});
+		$(".calc__hidden").css({display: "block"});
+	});
+
+	$('input[name=calc_language]').change(function(){
+
+		var current_lang = $('input[name=calc_language]:checked').val();
+
+		$('.calc__title').css({display: "none"});
+		$('.calc__header').css({display: "none"});
+
+		var class_title = ".calc__title--" + current_lang;
+		var class_head = ".calc__header--" + current_lang;
+
+		$(class_title).css({display: "block"});
+		$(class_head).css({display: "block"});
+
+	});
 
 
 	$('input').change(function(){
 
+		var date_biggest = 0;
+		$('.calc__input--date').each(function(){
+
+			if( $(this).val() != "" ){
+				var date_biggest_msec = new Date( $(this).val() );
+				if ( date_biggest_msec.getTime() > date_biggest ){
+					date_biggest = date_biggest_msec.getTime();
+				}
+			}
+
+			console.log(date_biggest);
+			
+
+			var time = new Date(date_biggest);
+
+			var timeD = time.getDate();
+			var timeM = time.getMonth() + 1;
+			var timeY = time.getFullYear();
+
+			if ( timeM < 10 ) {timeM = "0" + timeM;}
+			if ( timeD < 10 ) {timeD = "0" + timeD;}
+
+			$('#inp1').val( timeY + "-" + timeM + "-" + timeD );
+
+		});
+
 		var inp1 = $('#inp1').val();
 
 		var date_check = new Date(inp1);
-		var date_90 = Math.ceil(Math.abs(date_check.getTime() - (24 * 3600 * 1000 * (90 + 1) ) ));
-		var date_180 = Math.ceil(Math.abs(date_check.getTime() - (24 * 3600 * 1000 * (180 + 1) ) ));
+		var date_90 = Math.ceil(Math.abs(date_check.getTime() - (24 * 3600 * 1000 * (90 - 1) ) ));
+		var date_180 = Math.ceil(Math.abs(date_check.getTime() - (24 * 3600 * 1000 * (180 - 1) ) ));
 
 		console.log( new Date(date_90) );
 		console.log( new Date(date_180) );
@@ -103,10 +148,35 @@ $(document).ready(function() {
 
 		var count_itog = 90 - +count_summ;
 
+		var calc_lang = $('input[name=calc_language]:checked').val();
+
+		if ( calc_lang == "rus" ) { 
+			var calc_lang_text = "Ваш возможный срок пребывания (дней) — ";
+			var calc_lang_text_bad = "Cрок пребывания превышен (дней) — ";
+		}
+		if ( calc_lang == "eng" ) { 
+			var calc_lang_text = "Your possible length of stay (days) — ";
+			var calc_lang_text_bad = "Length of stay exceeded (days) — ";
+		}
+		if ( calc_lang == "spn" ) { 
+			var calc_lang_text = "Posible duración de la estancia (días) — ";
+			var calc_lang_text_bad = "Duración de la estancia excedida (días) — ";
+		}
+		if ( calc_lang == "frn" ) { 
+			var calc_lang_text = "Durée de séjour possible (jours) — ";
+			var calc_lang_text_bad = "Durée de séjour dépassée (jours) — ";
+		}
+		if ( calc_lang == "trk" ) { 
+			var calc_lang_text = "Olası kalış süreniz (gün) — ";
+			var calc_lang_text_bad = "Kalış süresi aşıldı (gün) — ";
+		}
+
 		if ( count_itog >= 0 ) {
-			$('.calc__result').text( "Ваш возможный срок пребывания (дней) — " + count_itog );
+			$('.calc__result').text( calc_lang_text + count_itog );
+			$('.calc__result').removeClass("calc__result--bad");
 		} else {
-			$('.calc__result').text( "Ваш возможный срок пребывания (дней) — " + 0 );
+			$('.calc__result').text( calc_lang_text_bad + (count_itog * (-1) ) );
+			$('.calc__result').addClass("calc__result--bad");
 		}
 
 	});
