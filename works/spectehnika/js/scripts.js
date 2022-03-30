4752bb0b8d805ab7eb8ff49d513710f35786b1de
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2022-03-30 14:47:41 +0600

    spectehnika
    30.03.2022
    14.47

--- works/spectehnika/js/scripts.js
+++ works/spectehnika/js/scripts.js
@@ -15,12 +15,25 @@ $(document).ready(function() {
 	$( "#calc_kolvoSmen" ).val($( "#calc_slider" ).slider( "value" ));
 	
 	$('#calc_kolvoSmen').on("change", function(){
+		if ( $(this).val() > 120 ) {
+			$(this).val(120);
+		}
+		if ( $(this).val() < 1 ) {
+			$(this).val(1);
+		}
 		var sliderInput = $(this).val();
 		$( "#calc_slider" ).slider( "value", sliderInput );
 		calculation();
 	});
 	
 
+	$('.calc__btn').click(function(){
+		$('.calc__window').addClass("calc__window--open");
+	});
+
+	$('.calc__close').click(function(){
+		$('.calc__window').removeClass("calc__window--open");
+	});
 
 	$('#calc_distance').change(calculation);
 
@@ -29,6 +42,8 @@ $(document).ready(function() {
 	function calculation() {
 
 		var calc_delivery = $('#calc_distance').val();
+
+		var calc_distance = $('#calc_distance option:selected').text();
 
 		$('#calc_delivery').text(calc_delivery);
 
@@ -41,6 +56,42 @@ $(document).ready(function() {
 		$('#calc_itog').text( "Количество смен (" + calc_kolvoSmen + ") x Аренда техники (" + calc_smena.toLocaleString("ru") + " ₽) + Доставка (" + calc_delivery + " ₽) = " + calc_sum.toLocaleString("ru") + " ₽" );
 
 		$('#calc_price').text( calc_sum.toLocaleString("ru") + " ₽" );
+
+		$('#calc_data').val(
+			"Стоимость смены: " + calc_smena + " ₽" + " <br>" + " \n" +
+			"Количество смен: " + calc_kolvoSmen + " <br>" + " \n" +
+			"Дистанция: " + calc_distance + " <br>" + " \n" +
+			"Итого: " + calc_sum.toLocaleString("ru") + " ₽"
+		);
+
+
+		if ( calc_kolvoSmen < 4 ) {
+			$('#calc_economy').text("0 ₽");
+		}
+		if ( calc_kolvoSmen >= 4 ) {
+			$('#calc_economy').text( calc_kolvoSmen * 500 + " ₽");
+		}
+		if ( calc_kolvoSmen >= 8 ) {
+			$('#calc_economy').text( calc_kolvoSmen * 1000 + " ₽");
+		}
+		if ( calc_kolvoSmen >= 20 ) {
+			$('#calc_economy').text( calc_kolvoSmen * 1300 + " ₽");
+		}
+
+
+		if ( calc_kolvoSmen >= 20 ) {
+			$('#calc_note').text("");
+		} 
+		if ( calc_kolvoSmen < 20 ) {
+			$('#calc_note').html(`А при оплате сразу за 20 смен стоимость аренды с доставкой техники выйдет дешевле на <b>26 000 ₽</b>`);
+		} 
+		if ( calc_kolvoSmen < 7 ) {
+			$('#calc_note').html(`А при оплате сразу за 8 смен стоимость аренды с доставкой техники выйдет дешевле на <b>8 000 ₽</b>`);
+		}
+		if ( calc_kolvoSmen < 4 ) {
+			$('#calc_note').html(`А при оплате сразу за 4 смены стоимость аренды с доставкой техники выйдет дешевле на <b>2 000 ₽</b>`);
+		}
+
 
 	}
 
