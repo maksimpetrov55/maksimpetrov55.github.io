a9f5e993e752e0d85da7a244d2e35bb7f8e3a85e
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2022-08-13 02:08:17 +0600

    Update ctrl3.js

--- control/ctrl3.js
+++ control/ctrl3.js
@@ -1,5 +1,4 @@
   var length = document.getElementsByClassName('calc__input').length;
   for (i = 0; i < length; i++){
-    
-    document.getElementsByClassName('calc__input')[i].id = 'calc_C' + +i + 2;
+    document.getElementsByClassName('calc__input')[i].id = 'calc_C' + (+i + 2);
   }
