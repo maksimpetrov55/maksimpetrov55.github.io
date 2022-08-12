1a87e76a3638a7a8974e11151fb6e145d187d334
maksimpetrov55 <38780830+maksimpetrov55@users.noreply.github.com>
2022-08-13 01:47:38 +0600

    ctrl3

--- control/ctrl3.js
+++ control/ctrl3.js
@@ -1,5 +1,5 @@
   var length = document.getElementsByClassName('calc__input').length;
   for (i = 0; i < length; i++){
-    document.getElementsByClassName('calc__input')[i].type = 'number';
+    
     document.getElementsByClassName('calc__input')[i].id = 'calc_C' + +i + 2;
   }
